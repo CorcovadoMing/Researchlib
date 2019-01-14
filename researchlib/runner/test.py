a84d22983bcc0ce9ac522066c63f1d2b5d710a74
Nick Tai <ntai@nvidia.com>
2019-01-14 10:32:26 +0800

    Refine test script

--- researchlib/runner/test.py
+++ researchlib/runner/test.py
@@ -1,50 +1,46 @@
 import torch
 import torch.nn.functional as F
 
-def test(model, test_loader, loss_fn, is_cuda, require_long, require_data, keep_x_shape, keep_y_shape, metrics):
-    model.eval()
+def test(**kwargs):
+    '''
+        kwargs: model, test_loader, loss_fn, is_cuda, require_long, require_data, keep_x_shape, keep_y_shape, metrics
+    '''
+    
+    kwargs['model'].eval()
     test_loss = 0
-    correct = 0
-    total = 0
     
     # Reset metrics
-    for m in metrics: m.reset()
+    for m in kwargs['metrics']: m.reset()
     
     with torch.no_grad():
-        for data, target in test_loader:
-            if require_long: target = target.long()
-            if is_cuda: data, target = data.cuda(), target.cuda()
+        for data, target in kwargs['test_loader']:
+            if kwargs['require_long']: target = target.long()
+            if kwargs['is_cuda']: data, target = data.cuda(), target.cuda()
             
-            output = model(data)
+            output = kwargs['model'](data)
             
             loss_input = [output, target]
             
-            if require_data: loss_input.append(data)
-            if not keep_x_shape: loss_input[0] = loss_input[0].contiguous().view(-1, loss_input[0].size(-1))
+            if kwargs['require_data']: loss_input.append(data)
+            if not kwargs['keep_x_shape']: loss_input[0] = loss_input[0].contiguous().view(-1, loss_input[0].size(-1))
             
-            if not keep_y_shape: 
+            if not kwargs['keep_y_shape']: 
                 loss_input[1] = loss_input[1].contiguous().view(-1)
             else:
                 loss_input[1] = loss_input[1].contiguous().view(-1, loss_input[1].size(-1))
             
-            test_loss += loss_fn(*loss_input).item()
+            test_loss += kwargs['loss_fn'](*loss_input).item()
             
-            # Capsule
+            # Capsule, TODO: refine the part
             if type(loss_input[0]) == type(()):
                 loss_input[0] = loss_input[0][0]
                 loss_input[0] = torch.sqrt((loss_input[0]**2).sum(dim=2, keepdim=True))
             
-#             total += len(loss_input[1])
-#             pred = loss_input[0].max(1, keepdim=True)[1] # get the index of the max log-probability
-#             correct += pred.eq(loss_input[1].view_as(pred)).sum().item()
-            for m in metrics: m.forward(output, target)
+            for m in kwargs['metrics']: m.forward(output, target)
     
     # Output metrics
-    for m in metrics: m.output()    
-# test_loss /= len(test_loader.dataset)
-
-#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-#        test_loss, correct, total,
-#        100. * correct / total))
-#     print('\nTest set: Average loss: {:.4f}'.format(
-#         test_loss))
+    for m in kwargs['metrics']: m.output()    
+    
+    test_loss /= len(kwargs['test_loader'].dataset)
+    print('\nTest set: Average loss: {:.4f}'.format(test_loss))
+    
