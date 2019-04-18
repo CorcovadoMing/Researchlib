7b154a6ee76282e30a86640e3ed95b1c770ee78b
Ming <rf37535@gmail.com>
2019-04-18 13:20:33 +0800

    Fix joint learning with multiple losses

--- researchlib/runner/runner.py
+++ researchlib/runner/runner.py
@@ -69,9 +69,9 @@ class Runner:
             for lf in loss_fn:
                 _loss_fn, require_long_, keep_x_shape_, keep_y_shape_, self.default_metrics = _process_loss_fn(lf)
                 self.loss_fn.append(_loss_fn)
-                self.require_long_.append(require_long_)
-                self.keep_x_shape_.append(keep_x_shape_)
-                self.keep_y_shape_.append(keep_y_shape_)
+                self.require_long_ += require_long_
+                self.keep_x_shape_ += keep_x_shape_
+                self.keep_y_shape_ += keep_y_shape_
         else:
             _loss_fn, require_long_, keep_x_shape_, keep_y_shape_, self.default_metrics = _process_loss_fn(loss_fn)
             self.loss_fn.append(_loss_fn)
