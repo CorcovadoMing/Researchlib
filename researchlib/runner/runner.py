9d8ddebf1a589b90d794d396fe08d989f5f54a24
Ming <rf37535@gmail.com>
2019-05-11 19:19:49 +0800

    Refactor the CAM

--- researchlib/runner/runner.py
+++ researchlib/runner/runner.py
@@ -1,33 +1,35 @@
-from .train import train_fn
-from .test import test_fn
 from .history import *
 from ..utils import *
 from ..metrics import *
 from ..loss import *
 from ..layers import *
-from .save_load import _save_model, _load_model
-import torch
-import torch.nn.functional as F
-from torch import nn
 from torch.optim import *
-import torch.backends.cudnn as cudnn
-from apex import amp
 from ..models import *
-
 from ..callbacks import *
-from ..utils import _add_methods_from, _get_iteration
 
 # -------------------------------------------------------
+from .train import train_fn
+from .test import test_fn
+from ..utils import _add_methods_from, _get_iteration
+from .save_load import _save_model, _load_model
+from torch.cuda import is_available
+from torch.nn import DataParallel
+import torch.backends.cudnn as cudnn
+from apex import amp
+
 from . import init_model
 from . import fit
+from . import cam
 
-@_add_methods_from(init_model, fit)
+@_add_methods_from(init_model)
+@_add_methods_from(fit)
+@_add_methods_from(cam)
 class Runner:
     def __init__(self, model=None, train_loader=None, test_loader=None, optimizer=None, loss_fn=None, reg_fn={}, reg_weights={}, monitor_mode='min', monitor_state='loss', fp16=False, multigpu=False):
         '''
             Multi-model supported
         '''
-        self.is_cuda = torch.cuda.is_available()
+        self.is_cuda = is_available()
         self.train_loader = train_loader
         self.test_loader = test_loader
         self.history_ = History()
@@ -143,7 +145,7 @@ class Runner:
         # Multi GPU
         self.multigpu = multigpu
         if self.multigpu:
-            self.model = nn.DataParallel(self.model)
+            self.model = DataParallel(self.model)
         
     
     def history(self, plot=True):
@@ -202,7 +204,7 @@ class Runner:
         self.model = _load_model(self.model, path, self.multigpu)
 
     def find_lr(self, mixup_alpha=0, plot=False, callbacks=[]):
-        _save_model(self.model, 'tmp.h5')
+        _save_model(self.model, 'find_lr_tmp.h5')
         try:
             loss, _ = self.trainer(model=self.model, 
                                 train_loader=self.train_loader, 
@@ -229,38 +231,4 @@ class Runner:
             if plot:
                 plot_utils(self.loss_history, self.lr_history)
         except Exception as e: print('Error:', e)
-        finally: self.model = _load_model(self.model, 'tmp.h5', self.multigpu)
-    
-    def cam(self, vx, final_layer, out_filters, classes):
-        if not self.cam_model:
-            self.cam_model = nn.Sequential(
-                    *list(self.model.children())[:final_layer+1], 
-                    nn.Conv2d(out_filters, classes, 1), 
-                    AdaptiveConcatPool2d(1),
-                    nn.Conv2d(classes*2, classes, 1),
-                    Flatten(),
-                    nn.LogSoftmax(1)
-                ).cuda()
-            self.cam_feature_layer = -5
-            module_trainable(self.cam_model[:self.cam_feature_layer], False)
-            #self.fit_onecycle()
-            r = Runner(self.cam_model, self.train_loader, self.test_loader, 'adam', 'nll', fp16=False)
-            r.fit(10, 1e-3)
-            
-        self.cam_feature = SaveFeatures(self.cam_model[self.cam_feature_layer])
-        py = self.cam_model(vx.cuda().float())
-        py = F.softmax(py, dim=-1)
-        py = py.detach().cpu().numpy()[0]
-        feat = self.cam_feature.features[0].detach().cpu().numpy()
-        feat = np.maximum(0, feat)
-        f2 = np.dot(np.rollaxis(feat,0,3), py)
-        f2 -= f2.min()
-        f2 /= f2.max()
-        dx = vx.cpu().numpy().transpose(0,2,3,1)[0]
-        #import skimage
-        plt.axis('off')
-        #plt.imshow(dx)
-        #ss = skimage.transform.resize(f2, dx.shape[:2])
-        plt.imshow(f2, alpha=0.5, cmap='hot')
-        module_trainable(self.model, True)
-        self.cam_feature.remove()
+        finally: self.model = _load_model(self.model, 'find_lr_tmp.h5', self.multigpu)
