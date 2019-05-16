a2b65ee3219c1a8e96db0e71995d74e4d10b80d3
Ming <rf37535@gmail.com>
2019-05-17 01:55:36 +0800

    Add openai gym wrapper

---
+++ researchlib/dataset/environment/env.py
@@ -0,0 +1,12 @@
+class Env:
+    def __init__(self):
+        pass
+    
+    def step(self):
+        raise('Not implemented')
+    
+    def reset(self):
+        raise('Not implemented')
+    
+    def render(self):
+        raise('Not implemented')
---
+++ researchlib/dataset/environment/gym.py
@@ -0,0 +1,26 @@
+from .env import Env
+import gym
+from IPython import display
+import matplotlib
+import matplotlib.pyplot as plt
+
+class GymEnv(Env):
+    def __init__(self, name):
+        super().__init__()
+        self.env = gym.make(name)
+        self.cache = None
+        self.reset()
+    
+    def step(self, action):
+        return self.env.step(action)
+    
+    def reset(self):
+        self.cache = None
+        return self.env.reset()
+    
+    def render(self):
+        if self.cache is None:
+            self.cache = plt.imshow(self.env.render(mode='rgb_array'))
+        self.cache.set_data(self.env.render(mode='rgb_array'))
+        display.display(plt.gcf())
+        display.clear_output(wait=True)
