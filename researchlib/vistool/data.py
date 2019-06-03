1da2238a5da132d45b8c8a3be79baa965dd01557
Ming <rf37535@gmail.com>
2019-06-04 00:59:08 +0800

    Fix the errors in image viewer

--- researchlib/vistool/data.py
+++ researchlib/vistool/data.py
@@ -1,3 +1,4 @@
+import torch
 from ipywidgets import interact
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,7 +9,7 @@ class _Image:
     
     def _browser_ui(self, index=0):
         data = self.data[index].numpy()
-        
+        gray = False
         if data.ndim < 3: gray = True
         else: data = data.transpose(1, 2, 0)
         
@@ -17,18 +18,30 @@ class _Image:
         print('Min', data.min())
         print('Max', data.max())
         
-        fig, arr = plt.subplots(1, 2)
-        if gray: arr[0].imshow(data, cmap='gray')
-        else: arr[0].imshow(data)
-        arr[1].hist(data)
-        asp = np.diff(arr[1].get_xlim())[0] / np.diff(arr[1].get_ylim())[0]
-        asp /= np.abs(np.diff(arr[0].get_xlim())[0] / np.diff(arr[0].get_ylim())[0])
-        arr[1].set_aspect(asp)
+        if gray:
+            fig, arr = plt.subplots(1, 2, figsize=(15, 15))
+            arr[0].imshow(data, cmap='gray')
+            arr[1].hist(data, histtype='step')
+            asp = np.diff(arr[1].get_xlim())[0] / np.diff(arr[1].get_ylim())[0]
+            asp /= np.abs(np.diff(arr[0].get_xlim())[0] / np.diff(arr[0].get_ylim())[0])
+            arr[1].set_aspect(asp)
+        else:
+            fig, arr = plt.subplots(1, 4, figsize=(15, 15))
+            arr[0].imshow(data)
+            for i in range(3):
+                arr[i+1].hist(data[:, :, i], histtype='step')
+                asp = np.diff(arr[i+1].get_xlim())[0] / np.diff(arr[i+1].get_ylim())[0]
+                asp /= np.abs(np.diff(arr[0].get_xlim())[0] / np.diff(arr[0].get_ylim())[0])
+                arr[i+1].set_aspect(asp)
         plt.tight_layout()
         plt.show()
         
     def viewer(self, loader):
-        self.data = loader.dataset.data
+        if type(loader) == tuple: loader = loader[0]
+        if type(loader.dataset) == torch.utils.data.dataset.TensorDataset:
+            self.data = loader.dataset.tensors[0]
+        else:
+            self.data = loader.dataset.data
         _ = interact(self._browser_ui, index=range(min(100, len(self.data))), continuous_update=False)
         
         
