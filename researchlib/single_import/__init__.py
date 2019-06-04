c0c5da66d618dfc80016d7278892c7fe7a2939a0
Ming <rf37535@gmail.com>
2019-06-04 15:11:56 +0800

    Now researchlib checks the GPU driver while loading

--- researchlib/single_import/__init__.py
+++ researchlib/single_import/__init__.py
@@ -10,3 +10,23 @@ from ..search import *
 from ..ml import *
 from ..pipeline import *
 from ..vistool import *
+
+
+from pynvml import *
+nvmlInit()
+
+deviceCount = nvmlDeviceGetCount()
+for i in range(deviceCount):
+    handle = nvmlDeviceGetHandleByIndex(i)
+    print("Using GPU", str(i)+":", nvmlDeviceGetName(handle).decode('utf-8'))
+
+print("Driver:", nvmlSystemGetDriverVersion().decode('utf-8'))
+
+# deviceCount = nvmlDeviceGetCount()
+# for i in range(deviceCount):
+#     handle = nvmlDeviceGetHandleByIndex(i)
+#     info = nvmlDeviceGetMemoryInfo(handle)
+#     print('Total:', info.total)
+#     print('Used:', info.used)
+#     s = nvmlDeviceGetUtilizationRates(handle)
+#     print(s.gpu, s.memory)
