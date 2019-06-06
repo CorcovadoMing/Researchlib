226772dd1d699315b56e2f42716fe3a41e55d1d6
Ming <rf37535@gmail.com>
2019-06-06 18:50:48 +0800

    Add fit_iteration training style

--- researchlib/runner/fit.py
+++ researchlib/runner/fit.py
@@ -6,9 +6,11 @@ from ..utils import _register_method, _get_iteration, set_lr
 from .history import History
 from ..models import GANModel
 from .save_load import _load_optimizer
+from itertools import cycle
 
 __methods__ = []
 register_method = _register_method(__methods__)
+
 
 @register_method
 def _set_cyclical(self, lr):
@@ -19,6 +21,7 @@ def _set_cyclical(self, lr):
         return [self.default_callbacks]
     else:
         return []
+
 
 @register_method
 def _set_sgdr(self, lr):
@@ -31,6 +34,7 @@ def _set_sgdr(self, lr):
     else:
         return []
 
+
 @register_method
 def _set_onecycle(self, lr):
     if self.default_callbacks:
@@ -40,6 +44,20 @@ def _set_onecycle(self, lr):
         return [self.default_callbacks]
     else:
         return []
+
+
+@register_method
+def fit_iteration(self, iteration, lr=1e-3, policy='cyclical', augmentor=None, mixup_alpha=0, metrics=[], callbacks=[], _id='none', self_iterative=False):
+    if policy == 'sc' or policy == 'superconverge':
+        callbacks = self._set_onecycle(lr) + callbacks
+    elif policy == 'cyclical':
+        callbacks = self._set_cyclical(lr) + callbacks
+    elif policy == 'cycle':
+        callbacks = self._set_sgdr(lr) + callbacks
+    elif policy == 'fixed':
+        set_lr(self.optimizer, lr)
+    self._fit(1, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self_iterative, cycle=True, total=iteration)
+
 
 @register_method
 def fit(self, epochs, lr=1e-3, policy='cyclical', augmentor=None, mixup_alpha=0, metrics=[], callbacks=[], _id='none', self_iterative=False):
@@ -55,7 +73,8 @@ def fit(self, epochs, lr=1e-3, policy='cyclical', augmentor=None, mixup_alpha=0,
     elif policy == 'fixed':
         set_lr(self.optimizer, lr)
         total_epochs = epochs
-    self._fit(total_epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self_iterative)
+    self._fit(total_epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self_iterative, cycle=False)
+
 
 @register_method
 def _process_type(self, data_pack):
@@ -77,6 +96,7 @@ def _process_type(self, data_pack):
     
     return data, target
     
+    
 @register_method
 def _process_data(self, data, target, augmentor, mixup_alpha):
     ''' INTERNAL_FUNCTION:
@@ -85,7 +105,6 @@ def _process_data(self, data, target, augmentor, mixup_alpha):
     
     def mixup_loss_fn(loss_fn, x, y, y_res, lam):
         return lam * loss_fn(x, y) + (1 - lam) * loss_fn(x, y_res)
-
     
     # On the fly augmentation
     if augmentor: data, target = augmentor.on(data, target)
@@ -110,13 +129,51 @@ def _process_data(self, data, target, augmentor, mixup_alpha):
 
 
 @register_method
-def _unload_data(self, data, target, target_res):
-    del data, target, target_res
+def _unload_data(self):
+    del self.data, self.target, self.target_res
     torch.cuda.empty_cache()
 
 
 @register_method
-def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self_iterative):
+def _fit_xy(self, augmentor, mixup_alpha, callbacks, metrics, loss_history, g_loss_history, d_loss_history, matrix_records, bar):
+    self.data, self.target, self.target_res = self._process_data(self.data, self.target, augmentor, mixup_alpha)
+    self.model.train()
+    self.trainer(model=self.model,
+                data=self.data,
+                target=self.target,
+                target_res=self.target_res,
+                optimizer=self.optimizer, 
+                loss_fn=self.loss_fn,
+                mixup_loss_fn=self.mixup_loss_fn,
+                reg_fn=self.reg_fn,
+                reg_weights=self.reg_weights,
+                epoch=self.epoch, 
+                keep_x_shape=self.keep_x_shape_,
+                keep_y_shape=self.keep_y_shape_,
+                mixup_alpha=mixup_alpha,
+                callbacks=callbacks,
+                metrics=metrics,
+                loss_history=loss_history,
+                g_loss_history=g_loss_history,
+                d_loss_history=d_loss_history,
+                matrix_records=matrix_records,
+                bar=bar)
+    self.model.eval()
+
+
+@register_method
+def _cycle(self, data, _cycle_flag=False):
+    if _cycle_flag:
+        return cycle(enumerate(data))
+    else:
+        return enumerate(data)
+        
+
+@register_method
+def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self_iterative, cycle, total=0):
+    if total == 0:
+        total = len(self.train_loader)
+        
     if len(self.experiment_name) == 0:
         self.start_experiment('default')
     
@@ -141,7 +198,7 @@ def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self
                 callback_func.on_epoch_begin(model=self.model, 
                                             train_loader=self.train_loader, 
                                             optimizer=self.optimizer,
-                                            epoch=epoch)
+                                            epoch=self.epoch)
 
             loss_history = []
             g_loss_history = []
@@ -149,34 +206,26 @@ def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self
             matrix_records = History()
 
             for m in metrics: m.reset()
-            bar = tqdm(self.train_loader, leave=False)
+            bar = tqdm(range(total), leave=False, initial=1)
 
-            for batch_idx, data_pack in enumerate(bar):
-                data, target = self._process_type(data_pack)
-                data, target, target_res = self._process_data(data, target, augmentor, mixup_alpha)
-                
-                self.model.train()
-                self.trainer(model=self.model,
-                            data=data,
-                            target=target,
-                            target_res=target_res,
-                            optimizer=self.optimizer, 
-                            loss_fn=self.loss_fn,
-                            mixup_loss_fn=self.mixup_loss_fn,
-                            reg_fn=self.reg_fn,
-                            reg_weights=self.reg_weights,
-                            epoch=epoch, 
-                            keep_x_shape=self.keep_x_shape_,
-                            keep_y_shape=self.keep_y_shape_,
-                            mixup_alpha=mixup_alpha,
-                            callbacks=callbacks,
-                            metrics=metrics,
-                            loss_history=loss_history,
-                            g_loss_history=g_loss_history,
-                            d_loss_history=d_loss_history,
-                            matrix_records=matrix_records,
-                            bar=bar)
-                self.model.eval()
+            iteration_break = total
+            for batch_idx, data_pack in self._cycle(self.train_loader, cycle):
+                self.data, self.target = self._process_type(data_pack)
+                self._fit_xy(augmentor, 
+                            mixup_alpha, 
+                            callbacks, 
+                            metrics, 
+                            loss_history, 
+                            g_loss_history, 
+                            d_loss_history, 
+                            matrix_records, 
+                            bar)
+                            
+                bar.update(1)
+                iteration_break -= 1
+                if iteration_break == 0:
+                    bar.close()
+                    break
                 
             # Output metrics
             for m in metrics: matrix_records.add(m.output(), prefix='train')
@@ -200,7 +249,7 @@ def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self
                                                             test_loader=self.test_loader, 
                                                             loss_fn=self.loss_fn, 
                                                             is_cuda=self.is_cuda,
-                                                            epoch=epoch,
+                                                            epoch=self.epoch,
                                                             require_long=self.require_long_,  
                                                             keep_x_shape=self.keep_x_shape_,
                                                             keep_y_shape=self.keep_y_shape_,
@@ -270,6 +319,6 @@ def _fit(self, epochs, lr, augmentor, mixup_alpha, metrics, callbacks, _id, self
                     except:
                         pass
             self.model.cpu()
-            self._unload_data(data, target, target_res)
+            self._unload_data()
             torch.cuda.empty_cache()
         # End unloading to CPU
