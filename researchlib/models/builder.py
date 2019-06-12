b24cd94e21b8ba9a023086cf6026d9bb7129c26d
Ming <rf37535@gmail.com>
2019-06-12 13:15:35 +0800

    Implement multiscale input/output

--- researchlib/models/builder.py
+++ researchlib/models/builder.py
@@ -1,30 +1,21 @@
 from torch import nn
-from ..wrapper import MultiscaleOutput
+from ..wrapper import MultiscaleOutput, MultiscaleInput
         
 class builder(nn.Module):
-    def __init__(self, nnlist, input_multiscale=False, return_multiscale=False):
+    # Static builder queue
+    queue = []
+    def __init__(self, nnlist):
         super().__init__()
-        self.return_multiscale = return_multiscale
-        self.input_multiscale = input_multiscale
         self.nnlist = nn.ModuleList(nnlist)
-    
+        
     def forward(self, x):
-        if self.return_multiscale:
-            out = []
-            final_append=False
-            for i in range(len(self.nnlist)):
+        for i in range(len(self.nnlist)):    
+            if type(self.nnlist[i]) == MultiscaleOutput:
                 x = self.nnlist[i](x)
-                if type(self.nnlist[i]) == MultiscaleOutput:
-                    out.append(x)
-                    final_append=True
-                else:
-                    final_append=False
-            if final_append:
-                return x, out[:-1]
+                builder.queue.append(x)
+            elif type(self.nnlist[i]) == MultiscaleInput:
+                x = self.nnlist[i](x, builder.queue.pop())
             else:
-                return x, out
-        else:
-            for i in range(len(self.nnlist)):
                 x = self.nnlist[i](x)
-            return x
+        return x
         
--- researchlib/wrapper/multiscale.py
+++ researchlib/wrapper/multiscale.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 class MultiscaleOutput(nn.Module):
@@ -6,4 +7,18 @@ class MultiscaleOutput(nn.Module):
         self.f = f
         
     def forward(self, x):
+        return self.f(x)
+        
+        
+class MultiscaleInput(nn.Module):
+    def __init__(self, f, type='concat'):
+        super().__init__()
+        self.f = f
+        self.type = type
+        
+    def forward(self, x, x_p):
+        if self.type == 'concat':
+            x = torch.cat([x, x_p], dim=1)
+        elif self.type == 'add':
+            x = x + x_p
         return self.f(x)
