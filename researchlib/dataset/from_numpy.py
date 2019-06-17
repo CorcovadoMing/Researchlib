1800c57c774f71f1af7f34c8ba6bced0308738ea
Ming <rf37535@gmail.com>
2019-06-17 21:58:39 +0800

    Fix imbalance sampler for FromNumpy

--- researchlib/dataset/from_numpy.py
+++ researchlib/dataset/from_numpy.py
@@ -4,13 +4,18 @@ import torch.utils.data as utils
 
 def FromNumpy(data, inputs=1, batch_size=1, shuffle=True, sampler=None, batch_sampler=None, num_workers=8, pin_memory=True, drop_last=False):
     data = [torch.from_numpy(i) for i in data]
-    dataset_ = utils.TensorDataset(*data)
-    dataset_loader_ = utils.DataLoader(dataset_,
-                                        batch_size=batch_size,
-                                        shuffle=shuffle,
-                                        sampler=sampler,
-                                        batch_sampler=batch_sampler,
-                                        num_workers=num_workers,
-                                        pin_memory=pin_memory,
-                                        drop_last=drop_last)
-    return dataset_loader_, inputs
+    _dataset = utils.TensorDataset(*data)
+    _shuffle = shuffle
+    _sampler = None
+    if sampler is not None:
+        _shuffle = False
+        _sampler = sampler(_dataset)
+    _loader = utils.DataLoader(_dataset,
+                                batch_size=batch_size,
+                                shuffle=_shuffle,
+                                sampler=_sampler,
+                                batch_sampler=batch_sampler,
+                                num_workers=num_workers,
+                                pin_memory=pin_memory,
+                                drop_last=drop_last)
+    return _loader, inputs
