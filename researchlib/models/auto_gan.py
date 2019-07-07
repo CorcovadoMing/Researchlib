Fix sample gradient

researchlib/models/auto_gan.py
@@ -14,7 +14,6 @@ class MinibatchDiscrimination(nn.Module):
         self.T = nn.Parameter(
             torch.Tensor(in_features, out_features - in_features, intermediate_features)
         )
-        nn.init.normal_(self.T)
 
     def forward(self, x):
         """Computes the output of the Minibatch Discrimination Layer
@@ -238,8 +237,8 @@ class AutoGAN_D(torch.nn.Module):
         main.add_module('End-Feature', ToFeature())
         # Size = (bs, base_hidden * mult)
         self.out = nn.Sequential(*[
-            MinibatchDiscrimination(base_hidden*mult, base_hidden*mult+32),
-            sn(nn.Linear(base_hidden*mult+32, 1))
+            #MinibatchDiscrimination(base_hidden*mult, base_hidden*mult+32),
+            sn(nn.Linear(base_hidden*mult, 1))
         ])
         
         self.main = main

researchlib/models/gan.py
@@ -34,23 +34,24 @@ class GANModel(nn.Module):
         return condition_data.to(device)
     
     def sample(self, bs, condition_data=None, inference=True, requires_grad=False, ema=False):
-        noise = torch.empty((bs, self.latent_vector_len)).normal_(0, 1)
-        if condition_data is not None:
-            if inference: condition_data = self._parse_condition_data(condition_data, self.condition_onehot, self.g_condition_vector_len)
-            noise = noise.to(condition_data.device)
-            noise = torch.cat([noise, condition_data], dim=1)
-        else:
-            if not inference:
-                noise = noise.cuda()
-        if ema:
-            ema_generator = pickle.loads(pickle.dumps(self.generator))
-            named_dict = dict(self.generator.named_parameters())
-            for name, p in ema_generator.named_parameters():
-                p.data.copy_(named_dict[name].ema.data)
-            fake = ema_generator(noise)
-        else:
-            fake = self.generator(noise)
-        return fake if requires_grad else fake.detach()
+        with torch.set_grad_enabled(requires_grad):
+            noise = torch.empty((bs, self.latent_vector_len)).normal_(0, 1)
+            if condition_data is not None:
+                if inference: condition_data = self._parse_condition_data(condition_data, self.condition_onehot, self.g_condition_vector_len)
+                noise = noise.to(condition_data.device)
+                noise = torch.cat([noise, condition_data], dim=1)
+            else:
+                if not inference:
+                    noise = noise.cuda()
+            if ema:
+                ema_generator = pickle.loads(pickle.dumps(self.generator))
+                named_dict = dict(self.generator.named_parameters())
+                for name, p in ema_generator.named_parameters():
+                    p.data.copy_(named_dict[name].ema.data)
+                fake = ema_generator(noise)
+            else:
+                fake = self.generator(noise)
+        return fake
     
     def forward_d(self, x, condition_data=None, ema=False):
         if condition_data is not None: