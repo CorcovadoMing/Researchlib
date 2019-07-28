118b0a3df351eecb6bfc78685b8eac177574dc9c
Ming <rf37535@gmail.com>
2019-07-28 20:36:29 +0800

    Complete the data parser for building dataset

--- researchlib/datatool/parser.py
+++ researchlib/datatool/parser.py
@@ -5,13 +5,18 @@ import re
 from imageio import imread
 import pandas as pd
 from tqdm.auto import tqdm
-
+from ..utils import ParallelExecutor
+import torch.multiprocessing as mp
+import cv2
+import math
+import zarr
 
 class _Parser:
     def __init__(self):
         pass
 
-    def build(self, name: str, path: str, parse_format: str, label_mapping: str = None, sep: str = None) -> None:
+    
+    def parse(self, name: str, path: str, parse_format: str, label_mapping: str = None, sep: str = None) -> None:
         os.makedirs(name, exist_ok=True)
 
         glob_pattern = os.path.join(
@@ -65,3 +70,45 @@ class _Parser:
         df = pd.DataFrame(list(zip(data_path, label)))
         df.columns = ['Data Path', 'Labels']
         df.to_csv(os.path.join(name, 'parse_result.csv'), index=False)
+    
+    
+    def build(self, name: str, shape, batch_size: int = 1000, num_workers: int = os.cpu_count()):
+        def _task(*args):
+            desc = args[0]
+            data_zarr = args[1]
+            label_zarr = args[2]
+            data_zarr[desc['id']] = cv2.resize(imread(desc['data'], pilmode='RGB'), (256, 256)).transpose((2,0,1))
+            label_zarr[desc['id']] = int(desc['label'])
+            return desc['id']
+        
+        parse_file = pd.read_csv(os.path.join(name, 'parse_result.csv'))
+        parse_iter = zip(parse_file['Data Path'].values, parse_file['Labels'].values)
+        total_length = len(parse_file['Data Path'].values)
+        total_epoch = math.ceil(total_length/batch_size)
+        
+        # Initialization
+        root = zarr.open(os.path.join(name, 'db.zarr'), mode='w')
+        data_zarr = root.zeros('data', shape=(1, *shape), chunks=(1, *shape), dtype='f')
+        label_zarr = root.zeros('label', shape=(1,), chunks=(1,), dtype='i')
+    
+        # resize the storage in the first place
+        data_zarr.resize(total_length, *shape)
+        label_zarr.resize(total_length,)
+        
+        executor = ParallelExecutor(_task, max_job=batch_size, num_workers=num_workers)
+        executor.start(data_zarr, label_zarr)
+        
+        try:
+            executor.start(data_zarr, label_zarr)
+            for i in tqdm(range(total_epoch)):
+                for j in range(batch_size):
+                    try:
+                        data, label = next(parse_iter)
+                        executor.put({'id': (i * batch_size) + j, 'data': data, 'label': label})
+                    except:
+                        continue
+                _ = executor.wait()
+        except:
+            pass
+        finally:
+            executor.stop(data_zarr, label_zarr)
--- researchlib/utils/parallel_executor.py
+++ researchlib/utils/parallel_executor.py
@@ -1,27 +1,32 @@
 import torch.multiprocessing as mp
 from IPython import display
+import os
+
 
 class ParallelExecutor:
-    def __init__(self, task, max_job: int = 10, num_workers: int = 8):
-        assert max_job > 0
+    def __init__(self, task, max_job: int = 10, num_workers: int = os.cpu_count()):
+        assert max_job > 0, 'max_job should be > 0'
         self.max_job = int(max_job)
         self.job_count = 0
         self.job = mp.Queue(self.max_job)
         self.reporter = mp.Queue(self.max_job)
         self.task = task
         self.num_workers = num_workers
+        self.worker_pool = []
     
     def _worker(self, *args):
-        while not self.job.empty():
-            desc = self.job.get()
-            product = self.task(desc, args)
-            self.reporter.put(product)
+        while True:
+            if not self.job.empty():
+                desc = self.job.get()
+                product = self.task(desc, *args)
+                self.reporter.put(product)
         
     def put(self, job):
         self.job.put(job)
         self.job_count += 1
     
     def batch_put(self, batch_job):
+        assert len(batch_job) <= self.max_job, 'Jobs are more than the queue, plase set larger max_job and try again'
         for job in batch_job:
             self.put(job)
     
@@ -33,8 +38,14 @@ class ParallelExecutor:
     
     def start(self, *args):
         for rank in range(self.num_workers):
-            p = mp.Process(target=self._worker, args=(args,))
+            p = mp.Process(target=self._worker, args=(*args,))
             p.start()
+            self.worker_pool.append(p)
+    
+    def stop(self, *args):
+        for i in self.worker_pool:
+            i.terminate()
+        self.worker_pool = []
     
     def wait(self):
         result = []
