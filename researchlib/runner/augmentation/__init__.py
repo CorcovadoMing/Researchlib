8845ca532bdb6d9a53890641bd47e6da54c53a18
Ming <rf37535@gmail.com>
2019-08-06 17:55:00 +0800

    Add grayscale augmentation

--- researchlib/runner/augmentation/__init__.py
+++ researchlib/runner/augmentation/__init__.py
@@ -1,2 +1,3 @@
 from .hflip import HFlip
 from .cutout import Cutout
+from .grayscale import Grayscale
---
+++ researchlib/runner/augmentation/grayscale.py
@@ -0,0 +1,24 @@
+from ..template import template
+from ...utils import mapping
+import numpy as np
+import torch
+
+class Grayscale(template.TorchAugmentation):
+    def __init__(self, prob=None, mag=None, include_y=False):
+        super().__init__()
+        self.include_y = include_y
+        self.prob = prob
+        self.mag = mag
+    
+    def _aug_fn(self, img):
+        gs = img.clone()
+        gs[0].mul_(0.299).add_(0.587, gs[1]).add_(0.114, gs[2])
+        gs[1].copy_(gs[0])
+        gs[2].copy_(gs[0])
+        return gs
+    
+    def forward_single(self, x, y, mag):
+        x = [ self._aug_fn(i) for i in x]
+        if self.include_y:
+            y = [ self._aug_fn(i) for i in y]
+        return x, y
