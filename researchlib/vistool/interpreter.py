2f0b54da9edaeba470f19145a13fe2251887c3ea
Ming <rf37535@gmail.com>
2019-08-07 15:00:43 +0800

    Implement grad-cam++ visualization

--- researchlib/vistool/interpreter.py
+++ researchlib/vistool/interpreter.py
@@ -232,12 +232,11 @@ class _Interpreter:
         model = pickle.loads(pickle.dumps(model))
         # Find last convolution
         model.apply(_register_conv2d)
-        output = model(img[None, :, :, :])
-        if type(model.nnlist[-1]) == nn.Sigmoid:
-            loss = F.binary_cross_entropy(output, label)
-        else:
-            loss = -output[0, label].log()
-        loss.backward()
+        var_img = Variable(img, requires_grad=True)
+        output = model(var_img[None, :, :, :])
+        grad_out = output.clone().fill_(0)
+        grad_out[0, label] = 1
+        _ = grad(output, var_img, grad_out)
 
         target_forward = hook_forward_buffer[-1]
         for i in hook_backward_buffer[0]:
@@ -251,10 +250,14 @@ class _Interpreter:
         target_gradient = F.adaptive_avg_pool2d(target_gradient, 1).squeeze()
         target_forward = target_forward.squeeze()
         target_output = target_forward * target_gradient[:, None, None]
-        target_output = F.relu(target_output)
         cam = target_output.sum(0)
+        if cam.sum() < 0:
+            cam *= -1
         cam -= cam.min()
         cam /= cam.max()
+        cam *= 2
+        cam -= 1
+        cam = F.relu(cam)
         cam = cv2.resize(cam.detach().numpy(), img.shape[1:])
 
         if plot:
@@ -283,6 +286,96 @@ class _Interpreter:
         else:
             return torch.from_numpy(cam) # We just want the output still a torch tensor
 
+        
+    def grad_cam_pp(self, model, img, label, plot=True):
+        '''
+            Grad-CAM++: Improved Visual Explanations for Deep Convolutional Networks
+            https://arxiv.org/abs/1710.11063
+
+            @input: classification model built from builder
+            @output: gradcam map
+        '''
+
+        hook_module = []
+        hook_forward_buffer = []
+        hook_backward_buffer = []
+
+        def backward_hook_fn(module, input, output):
+            hook_backward_buffer.append(input)
+
+        def forward_hook_fn(module, input, output):
+            hook_forward_buffer.append(output)
+
+        def _register_conv2d(m):
+            if type(m) == nn.Conv2d:
+                if 1 not in m.kernel_size: # avoid 1x1 conv
+                    b_handler = m.register_backward_hook(backward_hook_fn)
+                    f_handler = m.register_forward_hook(forward_hook_fn)
+                    hook_module.append(b_handler)
+                    hook_module.append(f_handler)
+
+        # Deep copy the model
+        model = pickle.loads(pickle.dumps(model))
+        # Find last convolution
+        model.apply(_register_conv2d)
+        var_img = Variable(img, requires_grad=True)
+        output = model(var_img[None, :, :, :])
+        grad_out = output.clone().fill_(0)
+        grad_out[0, label] = 1
+        _ = grad(output, var_img, grad_out)
+
+        target_forward = hook_forward_buffer[-1]
+        for i in hook_backward_buffer[0]:
+            try:
+                shape = i.shape
+            except:
+                continue
+            if target_forward.shape[:2] == shape[:2]:
+                target_gradient = i
+
+
+        alpha = torch.where(target_gradient > 0, torch.ones_like(target_gradient), torch.zeros_like(target_gradient))
+        alpha = 1 / (alpha.sum(-1).sum(-1) + 1e-7)
+        target_gradient = alpha[:, :, None, None] * F.relu(target_gradient)
+        target_gradient = target_gradient.sum(-1).sum(-1).squeeze()
+        target_forward = target_forward.squeeze()
+        target_output = target_forward * target_gradient[:, None, None]
+        cam = target_output.sum(0)
+        if cam.sum() < 0:
+            cam *= -1
+        cam -= cam.min()
+        cam /= cam.max()
+        cam *= 2
+        cam -= 1
+        cam = F.relu(cam)
+        cam = cv2.resize(cam.detach().numpy(), img.shape[1:])
+
+        if plot:
+            plot_img = img - img.min()
+            plot_img /= plot_img.max()
+            plot_img = plot_img.detach().numpy().transpose(1,2,0)
+            _, arr = plt.subplots(1, 3)
+            if plot_img.shape[-1] == 1:
+                arr[0].imshow(plot_img[:, :, 0], cmap='gray')
+            else:
+                arr[0].imshow(plot_img)
+            arr[0].set_title('Input')
+            arr[0].axis('off')
+            arr[1].imshow(cam, cmap='gray')
+            arr[1].set_title('Grad-CAM++')
+            arr[1].axis('off')
+            if plot_img.shape[-1] == 1:
+                arr[2].imshow(plot_img[:, :, 0], cmap='gray')
+            else:
+                arr[2].imshow(plot_img)
+            arr[2].imshow(cam, cmap='hot', alpha=0.3)
+            arr[2].set_title('Grad-CAM++ on input')
+            arr[2].axis('off')
+            plt.tight_layout()
+            plt.show()
+        else:
+            return torch.from_numpy(cam) # We just want the output still a torch tensor
+    
 
     def excitation_backprop(self, model, img, label, contrastive=False, plot=True):
         '''
