64c342be25cb382c4fe1c375b321a5681ed89d93
Ming <rf37535@gmail.com>
2019-08-13 02:21:08 +0800

    Fix the issue of deadlock queue

--- researchlib/runner/prefetch.py
+++ researchlib/runner/prefetch.py
@@ -1,4 +1,5 @@
 import threading
+import time
 import sys
 
 if sys.version_info >= (3, 0):
@@ -26,18 +27,19 @@ class BackgroundGenerator(threading.Thread):
         threading.Thread.__init__(self)
         self.queue = Queue.Queue(max_prefetch)
         self.generator = generator
-        self.daemon = True
         self.start()
 
     def run(self):
         for item in self.generator:
             self.queue.put(item)
-        self.queue.put(None)
 
     def next(self):
-        next_item = self.queue.get()
-        if next_item is None:
-            raise StopIteration
+        next_item = None
+        while next_item is None:
+            try:
+                next_item = self.queue.get_nowait()
+            except:
+                time.sleep(0.1)
         return next_item
 
     # Python 3 compatibility
@@ -46,16 +48,3 @@ class BackgroundGenerator(threading.Thread):
 
     def __iter__(self):
         return self
-
-
-#decorator
-class background:
-    def __init__(self, max_prefetch=1):
-        self.max_prefetch = max_prefetch
-
-    def __call__(self, gen):
-        def bg_generator(*args, **kwargs):
-            return BackgroundGenerator(gen(*args, **kwargs),
-                                       max_prefetch=self.max_prefetch)
-
-        return bg_generator
