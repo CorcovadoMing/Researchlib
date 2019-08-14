9676f3bf11ddfe59ef3bfd5387ab4087f73fe4a2
Ming <rf37535@gmail.com>
2019-08-15 01:08:46 +0800

    Write less codes for block customization

--- researchlib/blocks/_convblock.py
+++ researchlib/blocks/_convblock.py
@@ -4,31 +4,23 @@ from torch import nn
 
 class ConvBlock(_Block):
     def __postinit__(self):
-        kernel_size = self._get_param('kernel_size', 3)
-        stride = self._get_param('stride', 1)
-        padding = self._get_param('padding', 1)
-        groups = self._get_param('groups', 1)
-        dilation = self._get_param('dilation', 1)
-        bias = self._get_param('bias', False)
-        preact = self._get_param('preact', False)
+        # Parameters
         norm_type = self._get_param('norm_type', 'batchnorm')
         pool_type = self._get_param('pool_type', 'maxpool')
         pool_factor = self._get_param('pool_factor', 2)
-        
+        conv_args = self._get_conv_args()
         
         # Layers
-        conv_layer = self.op(self.in_dim, self.out_dim, kernel_size, stride, padding, dilation, groups, bias)
+        conv_layer = self.op(self.in_dim, self.out_dim, *conv_args)
         activator_layer = nn.ReLU() # TODO
-        pool_layer = self._get_pool_layer(preact, pool_type, pool_factor) if self.do_pool else None
-        norm_layer = self._get_norm_layer(preact, norm_type) if self.do_norm else None
+        pool_layer = self._get_pool_layer(pool_type, pool_factor) if self.do_pool else None
+        norm_layer = self._get_norm_layer(norm_type) if self.do_norm else None
         
-        if preact:
+        if self.preact:
             self.layers = [norm_layer, activator_layer, conv_layer, pool_layer]
         else:
-            self.layers = [conv_layer, norm_layer, activator_layer, pool_layer]
-        
+            self.layers = [conv_layer, norm_layer, activator_layer, pool_layer] 
         self.layers = nn.Sequential(*list(filter(None, self.layers)))
-        
         
     def forward(self, x):
         return self.layers(x)
--- researchlib/blocks/template/block.py
+++ researchlib/blocks/template/block.py
@@ -1,18 +1,30 @@
 from torch import nn
 
 class _Block(nn.Module):
-    def __init__(self, op, in_dim, out_dim, do_pool, do_norm, **kwargs):
+    def __init__(self, op, in_dim, out_dim, do_pool, do_norm, preact, **kwargs):
         super().__init__()
         self.op = op
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.do_pool = do_pool
         self.do_norm = do_norm
+        self.preact = preact
         self.kwargs = kwargs
+        
+        # Customize
         self.__postinit__()
     
     def __postinit__(self):
         pass
+    
+    def _get_conv_args(self):
+        kernel_size = self._get_param('kernel_size', 3)
+        stride = self._get_param('stride', 1)
+        padding = self._get_param('padding', 1)
+        dilation = self._get_param('dilation', 1)
+        groups = self._get_param('groups', 1)
+        bias = self._get_param('bias', False)
+        return kernel_size, stride, padding, dilation, groups, bias
     
     def _get_param(self, key, init_value):
         try:
@@ -21,13 +33,15 @@ class _Block(nn.Module):
         except:
             return init_value
     
-    def _get_norm_layer(self, preact, norm_type):
-        if preact:
+    def _get_norm_layer(self, norm_type):
+        # TODO
+        if self.preact:
             return nn.BatchNorm2d(self.in_dim)
         else:
             return nn.BatchNorm2d(self.out_dim)
     
-    def _get_pool_layer(self, preact, pool_type, pool_factor):
+    def _get_pool_layer(self, pool_type, pool_factor):
+        # TODO
         return nn.MaxPool2d(2)
         
     def forward(self, x):
