24e6ba72761aab9aca44ad56d1c73219263bcec9
Ming <rf37535@gmail.com>
2019-08-17 12:24:20 +0800

    Implement filter policy

--- researchlib/models/auto_conv_net.py
+++ researchlib/models/auto_conv_net.py
@@ -1,4 +1,5 @@
 import re
+import math
 from ..layers import layer
 from ..blocks import block
 from .builder import builder
@@ -33,12 +34,19 @@ def _get_dim_type(op):
 
 # =============================================================
 
+def _filter_policy(base_dim, block_group, cur_dim, total_blocks, policy):
+    if policy == 'default':
+        return base_dim * (2 ** (block_group - 1))
+    elif policy == 'pyramid':
+        return math.floor(cur_dim + 200 / total_blocks)
+    
 
 def AutoConvNet(op,
                 input_dim,
                 total_blocks,
                 type='vgg',
                 filters=(128, 1024),
+                filter_policy='default',
                 flatten=False,
                 preact=True,
                 pool_freq=1,
@@ -46,14 +54,13 @@ def AutoConvNet(op,
                 **kwargs):
 
     _op_type = _get_op_type(type)
-    start_filter, max_filter = filters
+    base_dim, max_dim = filters
+    block_group = 1
 
     layers = []
-
-    in_dim = input_dim
-    out_dim = start_filter
-
     
+    in_dim = input_dim
+    out_dim = base_dim
 
     if preact:
         print(in_dim, out_dim)
@@ -63,15 +70,19 @@ def AutoConvNet(op,
         in_dim = out_dim
 
     for i in range(total_blocks):
-        print(in_dim, out_dim)
         id = i + 1
         
         if id % pool_freq == 0:
             do_pool = True
-            if out_dim < max_filter and id != 1:
-                out_dim *= 2
+            block_group += 1
         else:
             do_pool = False
+        
+        if not preact and id == 1:
+            out_dim = base_dim
+        else:
+            out_dim = _filter_policy(base_dim, block_group, in_dim, total_blocks, filter_policy)
+        print(in_dim, out_dim)
         
         layers.append(
             _op_type(op,
