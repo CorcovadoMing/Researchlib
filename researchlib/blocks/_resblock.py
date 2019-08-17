55b4e7f728dcbe7575704708ed5c20eeb1472427
Ming <rf37535@gmail.com>
2019-08-17 13:21:30 +0800

    Implement pyramid-like structure

--- researchlib/blocks/_resblock.py
+++ researchlib/blocks/_resblock.py
@@ -1,9 +1,26 @@
 from .template.block import _Block
 from ..layers import layer
 from torch import nn
+import torch
 from ._convblock import ConvBlock
 import copy
 
+class _padding_shortcut(nn.Module):
+    def __init__(self, in_dim, out_dim, pool_layer):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.pool_layer = nn.Sequential(*list(filter(None, [pool_layer])))
+    
+    def forward(self, x):
+        x = self.pool_layer(x)
+        if self.in_dim >= self.out_dim:
+            return x[:, :self.out_dim]
+        else:
+            padding = torch.autograd.Variable(torch.zeros((x.size(0), self.out_dim - self.in_dim, x.size(2), x.size(3)), device=x.device))
+            return torch.cat((x, padding), 1)
+
+        
 class ResBlock(_Block):
     '''
         Deep Residual Learning for Image Recognition
@@ -45,13 +62,24 @@ class ResBlock(_Block):
                 ConvBlock(self.op, self.out_dim, self.out_dim, False, self.do_norm, self.preact, **second_custom_kwargs)
             )
         
-        shortcut_kernel_size = 2 if is_transpose and self.do_pool else 1
-        if self.in_dim != self.out_dim or self.do_pool:
-            custom_kwargs = self._get_custom_kwargs({'kenel_size': shortcut_kernel_size, 'stride': stride})
-            reduction_op = self.op(self.in_dim, self.out_dim, kernel_size=shortcut_kernel_size, stride=stride)
-        else:
-            reduction_op = None 
+        shortcut_type = self._get_param('shortcut', 'projection')
+        if shortcut_type not in ['projection', 'padding']:
+            raise('Shortcut type is not supported')
+        if shortcut_type == 'projection':
+            shortcut_kernel_size = 2 if is_transpose and self.do_pool else 1
+            if self.in_dim != self.out_dim or self.do_pool:
+                custom_kwargs = self._get_custom_kwargs({'kenel_size': shortcut_kernel_size, 'stride': stride})
+                reduction_op = self.op(self.in_dim, self.out_dim, kernel_size=shortcut_kernel_size, stride=stride)
+            else:
+                reduction_op = None 
+        elif shortcut_type == 'padding':
+            pool_type = self._get_param('pool_type', 'AvgPool') # As paper's design
+            pool_factor = self._get_param('pool_factor', 2)
+            pool_layer = self._get_pool_layer(pool_type, pool_factor) if self.do_pool else None
+            reduction_op = _padding_shortcut(self.in_dim, self.out_dim, pool_layer)
         self.shortcut = nn.Sequential(*list(filter(None, [reduction_op])))
+            
+            
 
         # Se
         self.se = self._get_param('se', True)
