a1965f78926663e4febfd04c2cc41e3c7880c84e
Ming <rf37535@gmail.com>
2019-08-24 15:00:19 +0800

    Add non_local unit

--- researchlib/blocks/unit/__init__.py
+++ researchlib/blocks/unit/__init__.py
@@ -1,9 +1,12 @@
 from .conv import _conv
+from .non_local import _non_local
 from .res2 import _res2
 from .randwire import _randwire
 
 
 class unit(object):
     conv = _conv
+    non_local = _non_local
     res2 = _res2
     randwire = _randwire
+    
