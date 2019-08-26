9fb919648fd9d588f652643419bd38e03c7fd23a
Ming <rf37535@gmail.com>
2019-08-26 16:08:33 +0800

    Generalize the padding shortcut

--- researchlib/blocks/_wide_resblock.py
+++ researchlib/blocks/_wide_resblock.py
@@ -1,29 +1,9 @@
 from .template.block import _Block
+from .template.shortcut import _padding_shortcut
 from ..layers import layer
 from torch import nn
 import torch
 from .unit import unit
-
-
-class _padding_shortcut(nn.Module):
-
-    def __init__(self, in_dim, out_dim, pool_layer):
-        super().__init__()
-        self.in_dim = in_dim
-        self.out_dim = out_dim
-        self.pool_layer = nn.Sequential(*list(filter(None, [pool_layer])))
-
-    def forward(self, x):
-        x = self.pool_layer(x)
-        if self.in_dim >= self.out_dim:
-            return x[:, :self.out_dim]
-        else:
-            return torch.cat((x,
-                              torch.autograd.Variable(
-                                  torch.zeros(
-                                      (x.size(0), self.out_dim - self.in_dim,
-                                       x.size(2), x.size(3)),
-                                      device=x.device))), 1)
 
 
 class WideResBlock(_Block):
---
+++ researchlib/blocks/template/shortcut.py
@@ -0,0 +1,21 @@
+from torch import nn
+import torch
+
+class _padding_shortcut(nn.Module):
+    def __init__(self, in_dim, out_dim, pool_layer):
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.pool_layer = nn.Sequential(*list(filter(None, [pool_layer])))
+
+    def forward(self, x):
+        x = self.pool_layer(x)
+        if self.in_dim >= self.out_dim:
+            return x[:, :self.out_dim]
+        else:
+            return torch.cat((x,
+                              torch.autograd.Variable(
+                                  torch.zeros(
+                                      (x.size(0), self.out_dim - self.in_dim,
+                                       *x.shape[2:]),
+                                      device=x.device))), 1)
