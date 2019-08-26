74c72fcb072470c69f903ad485ae53eea1a6e081
Ming <rf37535@gmail.com>
2019-08-26 19:09:39 +0800

    Add an option to toggle attention and set it default as False

--- researchlib/models/heads.py
+++ researchlib/models/heads.py
@@ -3,7 +3,7 @@ from .builder import builder
 from ..utils import ParameterManager
 from ..blocks.unit import unit
 
-def Heads(out_dim, **kwargs):
+def Heads(out_dim, attention=False, **kwargs):
     parameter_manager = ParameterManager(**kwargs)
     
     last_dim = parameter_manager.get_buffer('last_dim')
@@ -12,11 +12,12 @@ def Heads(out_dim, **kwargs):
     layers = [
         layer.__dict__['BatchNorm'+str(dim_type)](last_dim),
         layer.ReLU(),
-        layer.__dict__['DotNonLocalBlock'+str(dim_type)](last_dim),
+        layer.__dict__['DotNonLocalBlock'+str(dim_type)](last_dim) if attention else None,
         unit.conv(layer.__dict__['Conv'+str(dim_type)], last_dim , last_dim , False, True, False),
         layer.__dict__['AdaptiveMaxPool'+str(dim_type)](1),
         layer.Flatten(),
         layer.Linear(last_dim , out_dim)
     ]
     
+    layers = list(filter(None, layers))
     return builder(layers)
