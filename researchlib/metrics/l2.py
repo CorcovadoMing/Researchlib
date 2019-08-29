5f0d3f4c785e02e9d9edc933cc2a7d56fa19fefd
Ming <rf37535@gmail.com>
2019-08-29 18:34:15 +0800

    Fix L1/L2 matrix issue

--- researchlib/metrics/l2.py
+++ researchlib/metrics/l2.py
@@ -18,9 +18,9 @@ class L2(Matrix):
                                      (1 - lam) * y_true_res)).pow(2).mean()
             self.total += 1
         else:
-            y_pred, y_true = loss_input[0].cpu(), loss_input[1].cpu()
-            self.value += (y_pred - y_true).pow(2).mean()
-            self.total += 1
+            y_pred, y_true = loss_input[0].cpu().detach(), loss_input[1].cpu().detach()
+            self.value += (y_pred - y_true).pow(2).sum()
+            self.total += y_true.size(0)
 
     def output(self):
         mse = (self.value / float(self.total))
