32f4836ddb3945edb70f08dd77b2aa3b822b8e6e
Ming <rf37535@gmail.com>
2019-08-31 15:30:14 +0800

    Add cosine annealing with two phases policy

--- researchlib/utils/annealer.py
+++ researchlib/utils/annealer.py
@@ -2,17 +2,26 @@ import numpy as np
 
 
 def _Cosine(tcur, srange, tmax):
-        start, end = srange
-        if tcur <= tmax:
-            return end + (1/2) * (start - end) * (1 + np.cos(tcur/tmax * np.pi))
-        else:
-            return end
+    start, end = srange
+    if tcur <= tmax:
+        return end + (1/2) * (start - end) * (1 + np.cos(tcur/tmax * np.pi))
+    else:
+        return end
+
+def _CosineTwoWay(tcur, srange, tmax, split_ratio=0.333):
+    split_epoch = int(tmax * split_ratio)
+    if tcur <= split_epoch:
+        return _Cosine(tcur, srange, split_epoch)
+    else:
+        return _Cosine(tcur - split_epoch, srange[::-1], tmax - split_epoch)
+    
 
         
 class Annealer:
     tracker = {}
     
     Cosine = _Cosine
+    CosineTwoWay = _CosineTwoWay
     
     @classmethod
     def set_trace(cls, name, max_step, srange=[0, 1], anneal_when='iteration', anneal_fn=lambda x: x):
@@ -36,4 +45,3 @@ class Annealer:
             if cls.tracker[key]['anneal_when'] == 'epoch':
                 cls.tracker[key]['cur_step'] += 1
                 cls.tracker[key]['value'] = cls.tracker[key]['anneal_fn'](cls.tracker[key]['cur_step'], cls.tracker[key]['srange'], cls.tracker[key]['max_step'])
-
