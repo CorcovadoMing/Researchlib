34dfc901eb25e8b9350758be164e36c12b7d3e68
Ming <rf37535@gmail.com>
2019-09-15 17:29:07 +0800

    Default preact is False; Refine the Heads module in auto model

--- researchlib/models/heads.py
+++ researchlib/models/heads.py
@@ -4,21 +4,23 @@ from ..utils import ParameterManager
 from ..blocks.unit import unit
 
 
-def Heads(out_dim, attention=False, **kwargs):
+def Heads(out_dim, attention=False, preact=False, **kwargs):
     parameter_manager = ParameterManager(**kwargs)
 
     last_dim = parameter_manager.get_param('last_dim', None)
-    last_dim = last_dim if last_dim else parameter_manager.get_buffer(
-        'last_dim')
+    last_dim = last_dim if last_dim else parameter_manager.get_buffer('last_dim')
     dim_type = parameter_manager.get_buffer('dim_type')
 
     layers = [
-        layer.__dict__['BatchNorm' + str(dim_type)](last_dim),
-        layer.ReLU(),
-        layer.__dict__['DotNonLocalBlock' +
-                       str(dim_type)](last_dim) if attention else None,
-        unit.conv(layer.__dict__['Conv' + str(dim_type)], last_dim, last_dim,
-                  False, True, False) if attention else None,
+        # Preact
+        layer.__dict__['BatchNorm' + str(dim_type)](last_dim) if preact else None,
+        layer.ReLU() if preact else None,
+        
+        # Attention
+        layer.__dict__['DotNonLocalBlock' + str(dim_type)](last_dim) if attention else None,
+        unit.conv(layer.__dict__['Conv' + str(dim_type)], last_dim, last_dim, False, True, False) if attention else None,
+        
+        # Normal heads
         layer.__dict__['AdaptiveConcatPool' + str(dim_type)](1),
         layer.Flatten(),
         layer.Linear(2 * last_dim, out_dim)
