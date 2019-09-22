d662e1a1cd046b7148321ede93c291586a5095ea
Ming <rf37535@gmail.com>
2019-09-23 00:46:44 +0800

    Remove unsupported data loader

--- researchlib/dataset/__init__.py
+++ researchlib/dataset/__init__.py
@@ -1,22 +1,10 @@
 # Dataset Wrapper
 from .from_numpy import FromNumpy
-from .from_hdf5 import FromHDF5
 from .from_vision_dataset import VisionDataset
-from .from_text_dataset import TextDataset
-from .dali import FromDali
 from .environment import GymEnv
 
 # Dataset
 from .vision import _MNIST, _FashionMNIST, _KMNIST, _CIFAR10
-from .text import _AGNews
-
-# Sampler
-from .sampler import ImbalancedSampler
-
-
-class text(object):
-    AGNews = _AGNews
-
 
 class vision(object):
     MNIST = _MNIST
