3fa476faec902e1eadd8b8e976845a8cdde3fcd0
Ming <rf37535@gmail.com>
2019-09-24 02:54:08 +0800

    Update docker images to 19.09.1

--- Dockerfile/build.sh
+++ Dockerfile/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t rf37535/researchlib:19.09 . && docker push rf37535/researchlib:19.09 && docker tag rf37535/researchlib:19.09 rf37535/researchlib && docker push rf37535/researchlib
+docker build -t rf37535/researchlib:19.09.1 . && docker push rf37535/researchlib:19.09.1 && docker tag rf37535/researchlib:19.09.1 rf37535/researchlib && docker push rf37535/researchlib
 
