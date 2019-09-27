7663b155d09f8dea55ddb3eca6484fcfe052b676
Ming <rf37535@gmail.com>
2019-09-28 01:02:49 +0800

    Remove never used parameter

--- researchlib/utils/inifinity_loop.py
+++ researchlib/utils/inifinity_loop.py
@@ -1,8 +1,4 @@
-def inifinity_loop(loader, epoch=-1):
-    count = 0
+def inifinity_loop(loader):
     while True:
         for i, j in enumerate(loader):
             yield i, j
-        count += 1
-        if count == epoch:
-            break
