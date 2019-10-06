54f0d873aee27e1e36a483515cba9f212959d9ad
Ming <rf37535@gmail.com>
2019-10-07 04:09:18 +0800

    Unified the data loader naming

--- researchlib/dataset/__init__.py
+++ researchlib/dataset/__init__.py
@@ -6,12 +6,8 @@ from .vision import _CIFAR10, _NumpyDataset
 from .tf_dataset import _TFDataset
 from .torch_dataset import _TorchDataset
 
-class vision(object):
+class loader(object):
     CIFAR10 = _CIFAR10
     NumpyDataset = _NumpyDataset
-
-class general(object):
     TorchDataset = _TorchDataset
-    
-class thirdparty(object):
     TFDataset = _TFDataset
--- researchlib/datatool/datatool.py
+++ researchlib/datatool/datatool.py
@@ -1,8 +1,6 @@
 from .parser import _Parser
-from .loader import _Loader
 
 
 class _Datatool:
     def __init__(self):
         self.parser = _Parser()
-        self.loader = _Loader()
