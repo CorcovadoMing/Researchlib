33680e93fbd281b28c7555044aec93ee7b8b08d7
Ming <rf37535@gmail.com>
2019-10-10 03:13:34 +0800

    Use dawnbench version of smooth loss

--- researchlib/loss/mapping.py
+++ researchlib/loss/mapping.py
@@ -9,19 +9,11 @@ from torch.autograd import Variable
 import numpy as np
 
 
-class smooth_nll_loss(nn.Module):
-    def __init__(self, smoothing = 0.1):
-        super().__init__()
-        self.criterion = nn.KLDivLoss(reduction = 'batchmean')
-        self.smoothing = smoothing
-
-    def forward(self, x, target):
-        smooth_dist = x.data.clone()
-        smooth_dist.fill_(self.smoothing / (x.size(1) - 1))
-        smooth_dist.scatter_(1, target.data.view(-1, 1), 1 - self.smoothing)
-        return self.criterion(x, Variable(smooth_dist, requires_grad = False))
-
-
+def smooth_nll_loss(x, y, smoothing = 0.2):
+    kl = -x.mean(dim=-1)
+    xent = F.nll_loss(x, y, reduction='none')
+    return ((1-smoothing) * xent + smoothing * kl).mean()
+    
 def nl_loss(x, y):
     y = y.squeeze().long()
     return -x[range(y.shape[0]), y].log().mean()
@@ -43,7 +35,7 @@ def loss_mapping(loss_fn):
         default_metrics = [Acc()]
 
     elif loss_fn == 'smooth_nll':
-        loss_fn = smooth_nll_loss(0.1)
+        loss_fn = smooth_nll_loss
         default_metrics = [Acc()]
 
     elif loss_fn == 'bce':
