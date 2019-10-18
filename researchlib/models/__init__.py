5942ba568efc69986696141bcac39c8da3798e94
Ming <rf37535@gmail.com>
2019-10-18 16:24:40 +0800

    Add graph builder

--- researchlib/models/__init__.py
+++ researchlib/models/__init__.py
@@ -4,7 +4,7 @@ from .vae import VAEModel
 from .auto_conv_net import AutoConvNet
 from .auto_enc_dec import AutoEncDec
 from .heads import Heads
-
+from .graph_builder import GraphBuilder
 #from .auto_conv_net_2d import AutoConvNet2d, AutoConvTransposeNet2d
 #from .auto_unet_2d import AutoUNet2d
 #from .auto_gan import AutoGAN_G, AutoGAN_D, DCGAN_G, DCGAN_D, Discriminator, Generator
---
+++ researchlib/models/graph_builder.py
@@ -0,0 +1,47 @@
+from torch import nn
+
+has_inputs = lambda node: type(node) is tuple
+
+def path_iter(nested_dict, pfx=()):
+    for name, val in nested_dict.items():
+        if isinstance(val, dict): yield from path_iter(val, (*pfx, name))
+        else: yield ((*pfx, name), val)  
+
+            
+def normpath(path, sep='/'):
+    #simplified os.path.normpath
+    parts = []
+    for p in path.split(sep):
+        if p == '..': parts.pop()
+        elif p.startswith(sep): parts = [p]
+        else: parts.append(p)
+    return sep.join(parts)
+
+
+def pipeline(net, sep='/'):
+    return [(sep.join(path), (node if has_inputs(node) else (node, [-1]))) for (path, node) in path_iter(net)]
+
+
+def build_graph(net, sep='/'):
+    flattened = pipeline(net)
+    resolve_input = lambda rel_path, path, idx: normpath(sep.join((path, '..', rel_path))) if isinstance(rel_path, str) else flattened[idx+rel_path][0]
+    return {path: (node[0], [resolve_input(rel_path, path, idx) for rel_path in node[1]]) for idx, (path, node) in enumerate(flattened)} 
+
+
+class GraphBuilder(nn.Module):
+    def __init__(self, net):
+        super().__init__()
+        self.graph = build_graph(net)
+
+    def forward(self, inputs):
+        outputs = dict(inputs)
+        for k, (node, ins) in self.graph.items():
+            if k not in outputs:
+                inp = []
+                for x in ins:
+                    if type(outputs[x]) == tuple:
+                        inp += list(outputs[x])
+                    else:
+                        inp.append(outputs[x])
+                outputs[k] = node(*inp)
+        return outputs
