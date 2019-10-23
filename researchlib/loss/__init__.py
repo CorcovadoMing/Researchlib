3059aaa16819e31369139f3564dccea53db3ee73
Ming <rf37535@gmail.com>
2019-10-23 17:35:32 +0800

    Refactorized the loss structure

--- researchlib/loss/__init__.py
+++ researchlib/loss/__init__.py
@@ -1,6 +1,39 @@
-from .custom_loss import *
 from .ensemble import loss_ensemble
 from .mapping import *
-from .margin import *
-from .gan import GANLoss
-from .vae import VAELoss
+
+#from .gan import GANLoss
+#from .vae import VAELoss
+
+from .dice import DiceLoss, FlattenDiceLoss
+from .logcosh import LogCoshLoss
+from .huber import HuberLoss
+from .quantile import QuantileLoss
+from .ssim_msssim import SSIMLoss, MSSSIMLoss
+from .adaptive_robust_loss import AdaptiveRobustLoss
+from .soft_dtw import SoftDTWLoss
+
+class loss(object):
+    NegativeLogLikelyhood = nll_loss
+    NegativeLikelyhood = nl_loss
+    SmoothNegativeLogLikelyhood = smooth_nll_loss
+    AdaptiveRobustLoss = AdaptiveRobustLoss
+    DiceLoss = DiceLoss
+    FlattenDiceLoss = FlattenDiceLoss
+    LogCoshLoss = LogCoshLoss
+    HuberLoss = HuberLoss
+    QuantileLoss = QuantileLoss
+    SSIMLoss = SSIMLoss
+    MSSSIMLoss = MSSSIMLoss
+    SoftDTWLoss = SoftDTWLoss
+    
+#     'bce': F.binary_cross_entropy
+#     'adaptive_robust': AdaptiveRobustLoss(1),  # need to fix dimensions
+#     'focal': FocalLoss(),  # need to be refined
+#     'adaptive_focal': AdaptiveFocalLoss(),  # Experimental
+#     'margin': MarginLoss(),
+#     'mse': F.mse_loss,
+#     'l2': F.mse_loss,
+#     'mae': F.l1_loss,
+#     'l1': F.l1_loss,
+#     'huber': HuberLoss(),
+#     'logcosh': LogCoshLoss
--- researchlib/loss/mapping.py
+++ researchlib/loss/mapping.py
@@ -1,10 +1,6 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from .custom_loss import *
-from .margin import MarginLoss
-from torch.autograd import Variable
-import numpy as np
 
 
 def smooth_nll_loss(x, y, smoothing = 0.2):
@@ -30,10 +26,6 @@ def loss_mapping(loss_fn):
         'nl': nl_loss,
         'smooth_nll': smooth_nll_loss,
         'bce': F.binary_cross_entropy,
-        'adaptive_robust': AdaptiveRobustLoss(1),  # need to fix dimensions
-        'focal': FocalLoss(),  # need to be refined
-        'adaptive_focal': AdaptiveFocalLoss(),  # Experimental
-        'margin': MarginLoss(),
         'mse': F.mse_loss,
         'l2': F.mse_loss,
         'mae': F.l1_loss,
