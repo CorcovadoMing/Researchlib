667c0e94e453748ae4644bb1d25cb56801c1c73c
Ming <rf37535@gmail.com>
2019-10-31 18:01:26 +0800

    Scale the coordinates encoding to range [0, 1]

--- researchlib/ops/encoding/coordinates_encoding.py
+++ researchlib/ops/encoding/coordinates_encoding.py
@@ -9,8 +9,8 @@ class _CoordinatesEncoding2d(nn.Module):
 
     def forward(self, x):
         batch_size, _, image_height, image_width = x.size()
-        y_coords = torch.arange(image_height).unsqueeze(1).expand(image_height, image_width).float()
-        x_coords = torch.arange(image_width).unsqueeze(0).expand(image_height, image_width).float()
+        y_coords = torch.arange(image_height).unsqueeze(1).expand(image_height, image_width).float() / image_height
+        x_coords = torch.arange(image_width).unsqueeze(0).expand(image_height, image_width).float() / image_width
         coords = torch.stack((y_coords, x_coords), dim = 0)
         if self.with_r:
             rs = ((y_coords ** 2) + (x_coords ** 2)) ** 0.5
