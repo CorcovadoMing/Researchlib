b528228c378424477e776dc13545bbb0fcfd301e
Ming <rf37535@gmail.com>
2019-11-06 17:56:27 +0800

    Rename ExtendSequence for more precise name choice

--- researchlib/dataset/utils.py
+++ researchlib/dataset/utils.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 
-def _PaddingSequence(_y, value, max_length):
-    y = np.zeros((_y.shape[0], max_length))
-    y += value
-    y[:, :_y.shape[1]] = _y
-    return y.astype(np.int)
+def _ExtendSequence(_y, max_length, value = 0):
+    pad = np.zeros((_y.shape[0], max_length - _y.shape[1], *_y.shape[2:])) + value
+    print(_y.shape, pad.shape)
+    return np.concatenate([_y, pad], axis=1)
 
 
 class utils(object):
-    PaddingSequence = _PaddingSequence
+    ExtendSequence = _ExtendSequence
