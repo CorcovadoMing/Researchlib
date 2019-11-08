88cba4a5a317470356dad15d1647bbed79331320
Ming <rf37535@gmail.com>
2019-11-08 19:31:50 +0800

    Detach all metrics operation

--- researchlib/metrics/l1.py
+++ researchlib/metrics/l1.py
@@ -4,4 +4,5 @@ import torch
 
 class L1(namedtuple('L1', [])):
     def __call__(self, x, y):
+        x, y = x.detach(), y.detach()
         return torch.abs(x - y).sum().float() / x.size(0)
--- researchlib/metrics/l2.py
+++ researchlib/metrics/l2.py
@@ -4,4 +4,5 @@ import torch
 
 class L2(namedtuple('L2', [])):
     def __call__(self, x, y):
+        x, y = x.detach(), y.detach()
         return (x - y).pow(2).sum().float() / x.size(0)
