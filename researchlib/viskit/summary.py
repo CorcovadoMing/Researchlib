d7b9fad57dd866d06ce1e2f28e907de0ae8ce423
Ming <rf37535@gmail.com>
2019-11-14 10:59:18 +0800

    Improve the summary plot

--- researchlib/viskit/summary.py
+++ researchlib/viskit/summary.py
@@ -6,7 +6,7 @@ import numpy as np
 import pickle
 
 
-def _summary(experiment_name):
+def _summary(experiment_name, xscale='linear', yscale='linear'):
     count = 0
     arr = None
     exp_dir = os.path.join('checkpoint', 'experiments', str(experiment_name))
@@ -38,19 +38,29 @@ def _summary(experiment_name):
             mean, std = total.mean(0)[:, index_train], total.std(0)[:, index_train]
             arr[index, 0].plot(range(1, total.shape[1]+1), mean)
             arr[index, 0].fill_between(range(1, total.shape[1]+1),  # plot the two-sigma uncertainty about the mean
-                             (mean - 3.0 * std),
-                             (mean + 3.0 * std),
+                             (mean - 1.0 * std),
+                             (mean + 1.0 * std),
                              alpha=0.3)
             arr[index, 0].set_title(f'{name_col[index_train]}')
+            arr[index, 0].grid()
+            if 'acc' in name_col[index_train]:
+                arr[index, 0].set_yticks(np.linspace(0, 1, 11))
+            arr[index, 0].set_xscale(xscale)
+            arr[index, 0].set_yscale(yscale)
             
             index_val = index + split_col + 1
             mean, std = total.mean(0)[:, index_val], total.std(0)[:, index_val]
             arr[index, 1].plot(range(1, total.shape[1]+1), mean)
             arr[index, 1].fill_between(range(1, total.shape[1]+1),  # plot the two-sigma uncertainty about the mean
-                             (mean - 3.0 * std),
-                             (mean + 3.0 * std),
+                             (mean - 1.0 * std),
+                             (mean + 1.0 * std),
                              alpha=0.3)
             arr[index, 1].set_title(f'{name_col[index_val]}')
+            arr[index, 1].grid()
+            if 'acc' in name_col[index_val]:
+                arr[index, 1].set_yticks(np.linspace(0, 1, 11))
+            arr[index, 1].set_xscale(xscale)
+            arr[index, 1].set_yscale(yscale)
     
     if not len(path) and count == 0:
         print(f'{count} experiment found in "{experiment_name}"')
@@ -59,5 +69,6 @@ def _summary(experiment_name):
                          bbox_to_anchor=(0., 1.1, 2.2, .102), 
                          ncol=count, 
                          mode="expand", 
-                         borderaxespad=0)  
+                         borderaxespad=0)
+        
         plt.show()
