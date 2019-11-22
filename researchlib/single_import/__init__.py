659b338af546f9e9177cd398c90e76ce7868163a
Ming <rf37535@gmail.com>
2019-11-22 16:35:03 +0800

    Add logo

--- researchlib/single_import/__init__.py
+++ researchlib/single_import/__init__.py
@@ -24,6 +24,13 @@ from ..frontend.dashboard import _Dashboard
 import redis
 import pickle
 
+print(" ____                                     _      _      _  _      ")
+print("|  _ \  ___  ___   ___   __ _  _ __  ___ | |__  | |    (_)| |__   ")
+print("| |_) |/ _ \/ __| / _ \ / _` || '__|/ __|| '_ \ | |    | || '_ \  ")
+print("|  _ <|  __/\__ \|  __/| (_| || |  | (__ | | | || |___ | || |_) | ")
+print("|_|  \_\\___||___/ \___| \__,_||_|   \___||_| |_||_____||_||_.__/  ")
+print()
+
 # google api authorization
 uploader()
 
