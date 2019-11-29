f41f86e2d0610e94cbbb58284248094dd9005ac0
Ming <rf37535@gmail.com>
2019-11-29 13:10:23 +0800

    Implement the non-linear color jitter augmentation

--- researchlib/dataset/augmentations/__init__.py
+++ researchlib/dataset/augmentations/__init__.py
@@ -1,10 +1,11 @@
 from .crop import Crop
 from .hflip import HFlip
 from .cutout import Cutout
-
+from .nonlinear_jitter import NonlinearJitter
 
 class Augmentations(object):
     Crop = Crop
     HFlip = HFlip
     Cutout = Cutout
+    NonlinearJitter = NonlinearJitter
     
