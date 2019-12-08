d803eccce1f84d3e50ea52ba3a7f87abd6568252
Ming <rf37535@gmail.com>
2019-12-08 15:36:05 +0800

    Refine the torch dataset; Test on MNIST

--- researchlib/dataset/torch_dataset.py
+++ researchlib/dataset/torch_dataset.py
@@ -1,11 +1,12 @@
 import torchvision
+import torch
 from .np_dataset import _NumpyDataset
 
 
 def _TorchDataset(name, is_train, shuffle):
     dataset_fn = None
     for i in torchvision.datasets.__dict__:
-        if i.lower() == name:
+        if i.lower() == name and type(torchvision.datasets.__dict__[i]) == type:
             dataset_fn = torchvision.datasets.__dict__[i]
             break
 
@@ -13,4 +14,11 @@ def _TorchDataset(name, is_train, shuffle):
         raise ValueError(f'No dataset {name} founded')
 
     ds = dataset_fn(train = is_train, download = True, root = './data')
-    return _NumpyDataset(ds.data, ds.targets, shuffle, name = name)
+    
+    if name == 'mnist':
+        ds.data = ds.data.unsqueeze(-1)
+    
+    if type(ds.data) == torch.Tensor:
+        return _NumpyDataset(ds.data.numpy(), ds.targets.numpy(), shuffle, name = name)
+    else:
+        return _NumpyDataset(ds.data, ds.targets, shuffle, name = name)
