a8c5e6682fe6079a71bbfa2a0463bea15dfbc185
Ming <rf37535@gmail.com>
2019-12-19 00:54:52 +0800

    Fix the support set sampling

--- researchlib/dataset/general_dataset.py
+++ researchlib/dataset/general_dataset.py
@@ -18,6 +18,7 @@ class _GeneralLoader:
     def get_support_set(self, classes = [], shot = 5):
         collect = {k: [] for k in classes}
         g = self.get_generator(1, epochs = 3)
+        g.reset_state()
         count = 0
         for x, y in g:
             x = x[0]
@@ -27,5 +28,6 @@ class _GeneralLoader:
                 count += 1
             if count == len(classes) * shot:
                 break
+        del g
         return np.stack(list(collect.values())), np.repeat(np.array(list(collect.keys())),
                                                            shot).reshape(len(classes), shot)
--- researchlib/dataset/tf_dataset.py
+++ researchlib/dataset/tf_dataset.py
@@ -38,6 +38,7 @@ class _TFDataset:
     def get_support_set(self, classes = [], shot = 5):
         collect = {k: [] for k in classes}
         g = self.get_generator(1, epochs = 3)
+        g.reset_state()
         count = 0
         for x, y in g:
             x = x[0]
@@ -47,5 +48,6 @@ class _TFDataset:
                 count += 1
             if count == len(classes) * shot:
                 break
+        del g
         return np.stack(list(collect.values())), np.repeat(np.array(list(collect.keys())),
                                                            shot).reshape(len(classes), shot)
