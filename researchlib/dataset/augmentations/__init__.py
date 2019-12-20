4f977852de874bf67297501e11c336f69de90282
Ming <rf37535@gmail.com>
2019-12-20 09:16:11 +0800

    Add elastic transform augmentation

--- researchlib/dataset/augmentations/__init__.py
+++ researchlib/dataset/augmentations/__init__.py
@@ -2,10 +2,12 @@ from .crop import Crop
 from .hflip import HFlip
 from .cutout import Cutout
 from .nonlinear_jitter import NonlinearJitter
+from .elastic_transform import ElasticTransform
 
 class Augmentations(object):
     Crop = Crop
     HFlip = HFlip
     Cutout = Cutout
     NonlinearJitter = NonlinearJitter
+    ElasticTransform = ElasticTransform
     
