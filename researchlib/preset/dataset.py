743d79725f1dc9ce94f19b68644054878660da6f
Ming <rf37535@gmail.com>
2019-12-20 21:15:13 +0800

    Refine the dataset

--- researchlib/preset/dataset.py
+++ researchlib/preset/dataset.py
@@ -9,7 +9,7 @@ def CIFAR10(normalize=True):
         _normalize = Node('normalize', op.Normalize('static', (125.31, 122.95, 113.87), (62.99, 62.09, 66.70)), 'source')
     else:
         _normalize = Node('normalize', op.Normalize('static', (128, 128, 128), (128, 128, 128)), 'source')
-    _cifar10 = [
+    _source = [
         Node('source', op.Source(loader.TorchDataset('cifar10', True, True), loader.TorchDataset('cifar10', False, False))),
         _normalize,
         Node('preloop', op.Preloop(), 'normalize'),
@@ -18,7 +18,7 @@ def CIFAR10(normalize=True):
         Node('x', op.Name(), 'generator:0'),
         Node('y', op.Name(), 'generator:1'),
     ]
-    for i in _cifar10:
+    for i in _source:
         node, node_type = i
         graph.update(node)   
     return graph
@@ -30,7 +30,7 @@ def MNIST(normalize=True):
         _normalize = Node('normalize', op.Normalize('static', (33.3285,), (78.5655,)), 'source')
     else:
         _normalize = Node('normalize', op.Normalize('static', (128,), (128,)), 'source')
-    _mnist = [
+    _source = [
         Node('source', op.Source(loader.TorchDataset('mnist', True, True), loader.TorchDataset('mnist', False, False))),
         _normalize,
         Node('preloop', op.Preloop(), 'normalize'),
@@ -38,7 +38,7 @@ def MNIST(normalize=True):
         Node('x', op.Name(), 'generator:0'),
         Node('y', op.Name(), 'generator:1'),
     ]
-    for i in _mnist:
+    for i in _source:
         node, node_type = i
         graph.update(node)
     return graph
@@ -50,7 +50,7 @@ def ImageWoofFull(normalize=True, resize=None):
         _normalize = Node('normalize', op.Normalize('static', (128, 128, 128), (128, 128, 128)), 'source')
     else:
         _normalize = Node('normalize', op.Normalize('static', (128, 128, 128), (128, 128, 128)), 'source')
-    _mnist = [
+    _source = [
         Node('source', op.Source(loader.LFS.Classification.ImageWoofFull(True, True, resize), 
                                  loader.LFS.Classification.ImageWoofFull(False, False, resize))),
         _normalize,
@@ -60,15 +60,36 @@ def ImageWoofFull(normalize=True, resize=None):
         Node('x', op.Name(), 'generator:0'),
         Node('y', op.Name(), 'generator:1'),
     ]
-    for i in _mnist:
+    for i in _source:
         node, node_type = i
         graph.update(node)
     return graph
 
 
+def ImageNetteFull(normalize=True, resize=None):
+    graph = {}
+    if normalize:
+        _normalize = Node('normalize', op.Normalize('static', (128, 128, 128), (128, 128, 128)), 'source')
+    else:
+        _normalize = Node('normalize', op.Normalize('static', (128, 128, 128), (128, 128, 128)), 'source')
+    _source = [
+        Node('source', op.Source(loader.LFS.Classification.ImageNetteFull(True, True, resize), 
+                                 loader.LFS.Classification.ImageNetteFull(False, False, resize))),
+        _normalize,
+        #Node('preloop', op.Preloop(), 'normalize'),
+        Node('augmentation', op.Augmentation([Augmentations.Crop(resize, resize, resize//8), Augmentations.HFlip()]), 'normalize'),
+        Node('generator', op.Generator(), 'augmentation'),
+        Node('x', op.Name(), 'generator:0'),
+        Node('y', op.Name(), 'generator:1'),
+    ]
+    for i in _source:
+        node, node_type = i
+        graph.update(node)
+    return graph
 
 
 class Dataset(object):
     CIFAR10 = CIFAR10
     MNIST = MNIST
     ImageWoofFull = ImageWoofFull
+    ImageNetteFull = ImageNetteFull
