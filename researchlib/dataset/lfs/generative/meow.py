8cb5205903fce9a6da478f19242c4ef2bdc4c3fd
Ming <rf37535@gmail.com>
2019-12-20 11:22:59 +0800

    Add shuffle option in lfs generative loader

--- researchlib/dataset/lfs/generative/meow.py
+++ researchlib/dataset/lfs/generative/meow.py
@@ -5,14 +5,14 @@ import os
 
 this_dir, _ = os.path.split(__file__)
 
-def Meow32():
+def Meow32(shuffle = True):
     x = np.load(os.path.join(this_dir, 'meow32.npy'))
-    return _NumpyDataset(x, x.copy(), shuffle = True, name = '')
+    return _NumpyDataset(x, x.copy(), shuffle = shuffle, name = '')
                          
-def Meow64():
+def Meow64(shuffle = True):
     x = np.load(os.path.join(this_dir, 'meow64.npy'))
-    return _NumpyDataset(x, x.copy(), shuffle = True, name = '')
+    return _NumpyDataset(x, x.copy(), shuffle = shuffle, name = '')
 
-def Meow128():
+def Meow128(shuffle = True):
     x = np.load(os.path.join(this_dir, 'meow128.npy'))
-    return _NumpyDataset(x, x.copy(), shuffle = True, name = '')
+    return _NumpyDataset(x, x.copy(), shuffle = shuffle, name = '')
