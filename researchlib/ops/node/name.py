bb0a16cd1b89ef235ba61814861aecc07ac5e421
Ming <rf37535@gmail.com>
2019-12-24 20:15:33 +0800

    Name op is now with select_index option

--- researchlib/ops/node/name.py
+++ researchlib/ops/node/name.py
@@ -2,13 +2,16 @@ from torch import nn
 
 
 class _Name(nn.Module):
-    def __init__(self, detach = False):
+    def __init__(self, detach = False, select_index = None):
         super().__init__()
         self.detach = detach
+        self.select_index = select_index
     
     def forward(self, *x):
         if self.detach:
             x = [i.detach() for i in x]
+        if self.select_index is not None:
+            x = [i[self.select_index] for i in x]
         if len(x) == 1:
             return x[0]
         else:
