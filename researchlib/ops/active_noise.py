7692316f3a6a32ef48916231954a580227316292
Ming <rf37535@gmail.com>
2019-12-24 20:15:51 +0800

    Fix the active noise algorithm

--- researchlib/ops/active_noise.py
+++ researchlib/ops/active_noise.py
@@ -3,21 +3,36 @@ import torch
 
 
 class _ActiveNoise(nn.Module):
-    def __init__(self, channel, type='mixed', learnable=False):
+    def __init__(self, channel, size, mix_type='mixed', learn_type=None):
         super().__init__()
-        if learnable:
+        if learn_type == 'pixel':
+            self.alpha = nn.Parameter(torch.ones(1, channel, size, size))
+            self.beta = nn.Parameter(torch.ones(1, channel, size, size))
+        elif learn_type == 'channel':
             self.alpha = nn.Parameter(torch.ones(1, channel, 1, 1))
             self.beta = nn.Parameter(torch.ones(1, channel, 1, 1))
+        elif learn_type == 'sample':
+            self.alpha = nn.Parameter(torch.ones(1, 1, 1, 1))
+            self.beta = nn.Parameter(torch.ones(1, 1, 1, 1))
         else:
             self.alpha = 1
             self.beta = 1
-        self.type = type
+        self.mix_type = mix_type
+        self.learn_type = learn_type
     
     def forward(self, x):
+        x = x - x.mean([-1, -2], keepdim=True)
+        x = x / x.std([-1, -2], keepdim=True)
         if self.training:
-            noise = torch.empty_like(x).to(x.device)
-            if self.type == 'mul' or self.type == 'mixed':
-                return x * (noise.uniform_() + 0.5) * self.alpha.expand_as(x)
-            if self.type == 'add' or self.type == 'mixed':
-                return x + (noise.uniform_() - 0.5) * self.beta.expand_as(x)
+            if self.mix_type == 'mul' or self.mix_type == 'mixed':
+                mul_noise = torch.empty_like(x).to(x.device).normal_(0, 1)
+                mul_noise = torch.tanh(mul_noise) + 1
+                x = x * (mul_noise * self.alpha.expand_as(x))
+            if self.mix_type == 'add' or self.mix_type == 'mixed':
+                add_noise = torch.empty_like(x).to(x.device).normal_(0, 1)
+                add_noise = torch.tanh(add_noise)
+                x = x + (add_noise * self.beta.expand_as(x))
+        else:
+            if self.mix_type == 'mul' or self.mix_type == 'mixed':
+                x = x * self.alpha.expand_as(x)
         return x
