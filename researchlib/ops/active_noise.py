cbee12786b73933c6ed79afce5619f31dc639cc2
Ming <rf37535@gmail.com>
2019-12-27 18:11:40 +0800

    Calibrate the mean in active noise

--- researchlib/ops/active_noise.py
+++ researchlib/ops/active_noise.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-
+import torch.nn.functional as F
 
 class _ActiveNoise(nn.Module):
     def __init__(self):
@@ -9,5 +9,6 @@ class _ActiveNoise(nn.Module):
     def forward(self, x):
         if self.training:
             add_noise = torch.empty_like(x).to(x.device).normal_(0, 2*float(x.std()))
+            add_noise -= add_noise.mean(axis=list(range(2, add_noise.ndim)), keepdim=True).expand_as(add_noise) # mean calibration
             x = x + add_noise
         return x
