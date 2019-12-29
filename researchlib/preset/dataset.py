9885c3205e936c29c5fe723de64f9b420c382130
Ming <rf37535@gmail.com>
2019-12-29 16:21:32 +0800

    Add APIs to new datasets

--- researchlib/preset/dataset.py
+++ researchlib/preset/dataset.py
@@ -207,6 +207,58 @@ def RENOIR(normalize=True, resize=128):
     return graph
 
 
+def Test(name, normalize=True, resize=256):
+    graph = {}
+    if normalize:
+        _normalize = Node('normalize', op.Normalize('static', (128,), (128,)), 'source')
+    else:
+        _normalize = Node('normalize', op.Normalize('static', (128,), (128,)), 'source')
+    _source = [
+        Node('source', op.Source(
+#                                  loader.LFS.Restoration.Noise2d('speckle', True, True, resize), 
+                                 loader.LFS.Restoration.Test(name, True, resize),
+                                 loader.LFS.Restoration.Test(name, False, resize))),
+        _normalize,
+        #Node('preloop', op.Preloop(), 'normalize'),
+#         Node('augmentation', op.Augmentation([Augmentations.CircularCrop(resize, resize, resize//8),
+#                                              Augmentations.HFlip()]), 'normalize'),
+        Node('generator', op.Generator(), 'normalize'),
+        Node('x', op.Name(), 'generator:0'),
+        Node('y', op.Name(), 'generator:1'),
+    ]
+    for i in _source:
+        node, node_type = i
+        graph.update(node)
+    return graph
+
+
+def COIL100(name, normalize=True, resize=256):
+    graph = {}
+    if normalize:
+        _normalize = Node('normalize', op.Normalize('static', (128,), (128,)), 'source')
+    else:
+        _normalize = Node('normalize', op.Normalize('static', (128,), (128,)), 'source')
+    _source = [
+        Node('source', op.Source(
+#                                  loader.LFS.Restoration.Noise2d('speckle', True, True, resize), 
+                                 loader.LFS.Restoration.COIL100(name, True, resize),
+                                 loader.LFS.Restoration.COIL100(name, False, resize))),
+        _normalize,
+        #Node('preloop', op.Preloop(), 'normalize'),
+#         Node('augmentation', op.Augmentation([Augmentations.CircularCrop(resize, resize, resize//8),
+#                                              Augmentations.HFlip()]), 'normalize'),
+        Node('generator', op.Generator(), 'normalize'),
+        Node('x', op.Name(), 'generator:0'),
+        Node('y', op.Name(), 'generator:1'),
+    ]
+    for i in _source:
+        node, node_type = i
+        graph.update(node)
+    return graph
+
+
+
+
 class Dataset(object):
     CIFAR10 = CIFAR10
     MNIST = MNIST
@@ -218,3 +270,5 @@ class Dataset(object):
     
     BreastCancer = BreastCancer
     RENOIR = RENOIR
+    Test = Test
+    COIL100 = COIL100
