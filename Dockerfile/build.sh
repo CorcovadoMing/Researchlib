aaecdfb2084766b8b68817affe26cd7455912a4e
Ming <rf37535@gmail.com>
2019-12-29 16:47:15 +0800

    Update to 19.12

--- Dockerfile/build.sh
+++ Dockerfile/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t rf37535/researchlib:19.11.0 . && docker push rf37535/researchlib:19.11.0 && docker tag rf37535/researchlib:19.11.0 rf37535/researchlib && docker push rf37535/researchlib:latest
+docker build -t rf37535/researchlib:19.12.0 . && docker push rf37535/researchlib:19.12.0 && docker tag rf37535/researchlib:19.12.0 rf37535/researchlib && docker push rf37535/researchlib:latest
 
