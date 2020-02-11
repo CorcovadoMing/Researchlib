6c667d26c6f08d29a189f87aecbb0e2c3b79aa4e
Ming <rf37535@gmail.com>
2020-02-11 14:35:37 +0800

    Add SM3 optimizer

--- researchlib/preset/config/__init__.py
+++ researchlib/preset/config/__init__.py
@@ -1,7 +1,7 @@
 from .dawnfast import Dawnfast
 from .stepwise import Stepwise, Stepwise300
 from .poly import Poly
-from .cosine import Cosine, CosineForRandWireModel
+from .cosine import Cosine, Cosine300, CosineForRandWireModel
 
 
 class Config(object):
@@ -10,4 +10,5 @@ class Config(object):
     Stepwise300 = Stepwise300
     Poly = Poly
     Cosine = Cosine
+    Cosine300 = Cosine300
     CosineForRandWireModel = CosineForRandWireModel
--- researchlib/preset/config/cosine.py
+++ researchlib/preset/config/cosine.py
@@ -13,9 +13,24 @@ def Cosine(**kwargs):
     return default_kwargs
 
 
+def Cosine300(**kwargs):
+    default_kwargs = dict(
+        epochs=300, 
+        lr=1e-1, 
+        policy='cosine',
+        warmup=5,
+        batch_size=128,
+        accum_grad=1,
+        fp16=True,
+        init='xavier',
+    )
+    default_kwargs.update(kwargs)
+    return default_kwargs
+
+
 def CosineForRandWireModel(**kwargs):
     default_kwargs = dict(
-        epochs=150, 
+        epochs=300, 
         lr=1e-1, 
         policy='cosine',
         warmup=5,
