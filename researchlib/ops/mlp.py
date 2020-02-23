721ae73887712c4a9a85fcb1254262c0b8e4501e
Ming <rf37535@gmail.com>
2020-02-23 20:32:03 +0800

    Fix the MLP operation

--- researchlib/ops/mlp.py
+++ researchlib/ops/mlp.py
@@ -2,13 +2,13 @@ from torch import nn
     
 
 class _MLP(nn.Module):
-    def __init__(self, *layers, act=nn.ELU(), input_flatten=False):
+    def __init__(self, *layers, act=nn.ELU, input_flatten=False, end_act=False):
         super().__init__()
         _layers = []
         if input_flatten:
             _layers.append(nn.Flatten())
         for j in range(len(layers)-1):
-            _act = act if j < len(layers)-2 else None
+            _act = act() if j < len(layers)-2 or end_act else None
             _layers += [nn.Linear(layers[j], layers[j+1]), _act]
         self.net = nn.Sequential(*filter(None, _layers))
     
