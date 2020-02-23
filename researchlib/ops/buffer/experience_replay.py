fe08041b223abbb91339ded940d913b0e600cfcb
Ming <rf37535@gmail.com>
2020-02-24 04:30:38 +0800

    Fix the RND bugs

--- researchlib/ops/buffer/experience_replay.py
+++ researchlib/ops/buffer/experience_replay.py
@@ -8,7 +8,7 @@ class _ExperienceReplay(nn.Module):
         self.total_len = total_len
         self.samples_len = samples_len
         self.samples_poisson = samples_poisson
-        self.samples_len_type = 'dynamic' if samples_len > 0 else 'static'
+        self.samples_len_type = 'static' if samples_len >= 0 else 'dynamic'
         self.buffer = []
         self.cache = None
         self.enable = True
--- researchlib/ops/rl/actor_critic.py
+++ researchlib/ops/rl/actor_critic.py
@@ -7,17 +7,21 @@ from .utils import _discount
 
 class _ActorCritic(nn.Module):
     def __init__(self, agent,
-                 gae_gamma=0.99,
-                 gae_lambda=0.95,
+                 gae_ext_gamma=0.999,
+                 gae_ext_lambda=0.95,
+                 gae_int_gamma=0.99,
+                 gae_int_lambda=0.95,
                  vf_coeff=0.5, 
-                 int_coeff=1.0, 
+                 int_coeff=2.0, 
                  rdn_coeff=1.0, 
                  state_node='state', 
                  policy_node='policy'):
         super().__init__()
         self.eps = np.finfo(np.float32).eps.item()
-        self.gae_gamma = gae_gamma
-        self.gae_lambda = gae_lambda
+        self.gae_ext_gamma = gae_ext_gamma
+        self.gae_ext_lambda = gae_ext_lambda
+        self.gae_int_gamma = gae_int_gamma
+        self.gae_int_lambda = gae_int_lambda
         self.vf_coeff = vf_coeff
         self.int_coeff = int_coeff
         self.rdn_coeff = rdn_coeff
@@ -32,29 +36,37 @@ class _ActorCritic(nn.Module):
         result = self.agent({self.state_node: torch.stack(trajection['state'], 0).to(self.device)})
         action = torch.LongTensor(trajection['action']).to(self.device)
         logp = result[self.policy_node].log_prob(action).view(-1)
-        with torch.no_grad():
-            returns = torch.from_numpy(_discount(trajection['reward'], self.gae_gamma)).to(self.device).view(-1)
-            intrinsic = torch.zeros_like(returns) if 'intrinsic' not in trajection else trajection['intrinsic']
-            intrinsic = torch.from_numpy(_discount(intrinsic, self.gae_gamma)).to(self.device).view(-1)
         value_rollout = result['value'].view(-1)
-        intrinsic_rollout = torch.zeros_like(intrinsic) if 'intrinsic' not in trajection \
+        intrinsic_rollout = torch.zeros_like(logp) if 'intrinsic' not in trajection \
                                                             else result['intrinsic'].view(-1)
-        advantages_external = torch.FloatTensor(trajection['reward']).to(self.device) \
-                              + self.gae_gamma * torch.cat([value_rollout[1:], torch.zeros(1).to(self.device)]) \
-                              - value_rollout
-        advantages_external = torch.from_numpy(_discount(advantages_external, 
-                                                         self.gae_gamma * self.gae_lambda)).to(self.device)
-        advantages_internal = (intrinsic - intrinsic_rollout)
-        weights = advantages_external + advantages_internal
-        weights = (weights - weights.mean()) / (weights.std() + self.eps)
+        with torch.no_grad():
+            value_returns = torch.from_numpy(_discount(trajection['reward'], self.gae_ext_gamma)).to(self.device).view(-1)
+            intrinsic = torch.zeros_like(returns) if 'intrinsic' not in trajection else trajection['intrinsic']
+            intrinsic_returns = torch.from_numpy(_discount(intrinsic, self.gae_int_gamma)).to(self.device).view(-1)
+        
+            advantages_extrinsic = torch.FloatTensor(trajection['reward']).to(self.device) \
+                                  + self.gae_ext_gamma * torch.cat([value_rollout[1:], torch.zeros(1).to(self.device)]) \
+                                  - value_rollout
+            advantages_extrinsic = torch.from_numpy(_discount(advantages_extrinsic, 
+                                                             self.gae_ext_gamma * self.gae_ext_lambda)).to(self.device)
+
+            advantages_intrinsic = intrinsic.detach().to(self.device) \
+                                  + self.gae_int_gamma * torch.cat([intrinsic_rollout[1:], torch.zeros(1).to(self.device)]) \
+                                  - intrinsic_rollout
+            advantages_intrinsic = torch.from_numpy(_discount(advantages_intrinsic, 
+                                                             self.gae_int_gamma * self.gae_int_lambda)).to(self.device)
+            
+            weights = advantages_extrinsic + advantages_intrinsic
+            weights = (weights - weights.mean()) / (weights.std() + self.eps)
+            
         rnd_x = torch.zeros(1) if 'rnd_x' not in trajection else trajection['rnd_x']
         rnd_y = torch.zeros(1) if 'rnd_y' not in trajection else trajection['rnd_y']
         return (logp,
                 weights,
                 value_rollout,
-                returns,
+                value_returns,
                 intrinsic_rollout,
-                intrinsic,
+                intrinsic_returns,
                 rnd_x,
                 rnd_y)
     
