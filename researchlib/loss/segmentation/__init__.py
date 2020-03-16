17c03a8be8686effd116cf12fb44be3fe44866bd
CorcovadoMing <rf37535@gmail.com>
2020-03-16 04:40:54 +0000

    Add several segmentation loss

--- researchlib/loss/segmentation/__init__.py
+++ researchlib/loss/segmentation/__init__.py
@@ -1,5 +1,17 @@
 from .lovasz import LovaszSoftmaxLoss
+from .tverskey import BinaryTverskyLoss, FocalBinaryTverskyLoss, MultiTverskyLoss
+from .focal import BinaryFocalLoss
+from .dice import BinaryDiceLoss, DiceLoss, WBCELoss, WBCEDiceLoss
 
 
 class Segmentation(object):
     LovaszSoftmax = LovaszSoftmaxLoss
+    BinaryTversky = BinaryTverskyLoss
+    FocalBinaryTversky = FocalBinaryTverskyLoss
+    MultiTversky = MultiTverskyLoss
+    BinaryFocal = BinaryFocalLoss
+    BinaryDice = BinaryDiceLoss
+    Dice = DiceLoss
+    WBCE = WBCELoss
+    WBCEDice = WBCEDiceLoss
+    
