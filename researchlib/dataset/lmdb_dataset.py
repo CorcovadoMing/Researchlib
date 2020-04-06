da63a8b279aac87605433ff53a05d848591de999
CorcovadoMing <rf37535@gmail.com>
2020-04-06 06:01:02 +0000

    Update the LMDB loader

--- researchlib/dataset/lmdb_dataset.py
+++ researchlib/dataset/lmdb_dataset.py
@@ -2,7 +2,9 @@ from tensorpack.dataflow import *
 from .general_dataset import _GeneralLoader
 
 
-def _LMDBDataset(file_path, shuffle = True, name = ''):
-    df = LMDBSerializer.load(file_path, shuffle = shuffle)
+def _LMDBDataset(file_path, shuffle = True, name = '', shuffle_queue = 50000):
+    df = LMDBSerializer.load(file_path, shuffle = False)
+    if shuffle:
+        df = LocallyShuffleData(df, shuffle_queue)
     return _GeneralLoader(df, name)
     
