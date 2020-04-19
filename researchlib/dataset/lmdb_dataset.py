e5927d867b38d068ab945bdc3d5dfa2e560594a5
CorcovadoMing <rf37535@gmail.com>
2020-04-19 06:24:03 +0000

    Add decoder in LMDB loader

--- researchlib/dataset/lmdb_dataset.py
+++ researchlib/dataset/lmdb_dataset.py
@@ -1,9 +1,11 @@
 from tensorpack.dataflow import *
 from .general_dataset import _GeneralLoader
+import cv2
 
 
-def _LMDBDataset(file_path, shuffle = True, name = '', shuffle_queue = 50000):
-    df = LMDBSerializer.load(file_path, shuffle = False)
+def _LMDBDataset(file_path, shuffle = True, name = '', shuffle_queue = 50000, total_shuffle = False):
+    df = LMDBSerializer.load(file_path, shuffle = total_shuffle)
+    df = MapDataComponent(df, lambda x: cv2.imdecode(x, cv2.IMREAD_UNCHANGED), 0)
     if shuffle:
         df = LocallyShuffleData(df, shuffle_queue)
     return _GeneralLoader(df, name)
